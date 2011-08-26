7c570a9b50c07fc198a1523215e48eb825763f16
yotam.gi <yotam.gi@2154cc09-3a30-70b6-dd6e-86fc0e90be83>
2011-08-26 18:23:00 +0000

    Added the plane steering posebilities. Now it can be controlled by a nice servo interface.

--- trunk/src/simulator/plane.cc
+++ trunk/src/simulator/plane.cc
@@ -15,8 +15,12 @@ Plane::Plane(irr::IrrlichtDevice* device,
 		irr::core::vector3df start_pos,
 		const PlanesParams plane_params):
 		FlyingObject(device),
-		m_direction(irr::core::vector3df(0.0f, 0.0f, 1.0f))
+		m_direction(irr::core::vector3df(0.0f, 0.0f, -1.0f))
 {
+	m_servos.yaw_percentage = 50.0f;
+	m_servos.pitch_percentage = 50.0f;
+	m_servos.ailron_percentage = 50.0f;
+
 	irr::scene::ISceneManager* smgr = m_device->getSceneManager();
 
 	m_object = smgr->addMeshSceneNode(smgr->getMesh(plane_params.get_mesh_file().c_str()));
@@ -33,11 +37,34 @@ Plane::Plane(irr::IrrlichtDevice* device,
 
 void Plane::update(float time_delta) {
 	irr::core::vector3df pos = m_object->getPosition();
-	pos  += m_direction*time_delta*10.0f;
+	irr::core::vector3df rot = m_object->getRotation();
+	irr::core::vector3df dir = m_direction;
+
+	dir.rotateYZBy(rot.X);
+	dir.rotateXZBy(-1.*rot.Y);
+	dir.rotateXYBy(rot.Z);
+
+	pos  += dir*time_delta*30.0f;
+	rot += irr::core::vector3df(
+		((m_servos.pitch_percentage   - 50.0f)/50.0f)*30.0f*time_delta,
+		((m_servos.yaw_percentage 	  - 50.0f)/50.0f)*30.0f*time_delta,
+		((m_servos.ailron_percentage  - 50.0f)/50.0f)*30.0f*time_delta
+	);
+
 	m_object->setPosition(pos);
+	m_object->setRotation(rot);
+}
+
+void Plane::set_pitch_servo(float percentage) {
+	m_servos.pitch_percentage = percentage;
+}
+void Plane::set_yaw_servo(float percentage) {
+	m_servos.yaw_percentage = percentage;
+}
+void Plane::set_ailron_servo(float percentage) {
+	m_servos.ailron_percentage = percentage;
 }
 
 
-
-
 }  // namespace simulator
+
