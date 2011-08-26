ca46c9934d92beb88a59eb56af53319639ab8641
yotam.gi <yotam.gi@2154cc09-3a30-70b6-dd6e-86fc0e90be83>
2011-08-26 18:23:00 +0000

    Added the plane steering posebilities. Now it can be controlled by a nice servo interface.

--- src/simulator/plane.h
+++ src/simulator/plane.h
@@ -49,11 +49,24 @@ public:
 
 	virtual irr::core::vector3df get_pos() const { return m_object->getPosition(); }
 
+	// controlling the plane
+	void set_pitch_servo(float percentage);
+	void set_yaw_servo(float percentage);
+	void set_ailron_servo(float percentage);
+
 private:
 
 	irr::core::vector3df m_direction;
 
 	irr::scene::ISceneNode * m_object;
+
+	struct servo_states {
+		float yaw_percentage;
+		float pitch_percentage;
+		float ailron_percentage;
+	};
+
+	servo_states m_servos;
 
 };
 
