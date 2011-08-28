401cf9fb86d3846e649bbbb4a43514b7d1aa3f2f
yotam.gi <yotam.gi@2154cc09-3a30-70b6-dd6e-86fc0e90be83>
2011-08-28 18:51:00 +0000

    Improved the vector_t class. It is so butiful now!

--- trunk/src/autopilot/common/types.h
+++ trunk/src/autopilot/common/types.h
@@ -1,19 +1,29 @@
-/*
- * types.h
- *
- *  Created on: Aug 13, 2011
- *      Author: yotam
- */
-
 #ifndef TYPES_H_
 #define TYPES_H_
 
-struct vector_t {
+#include <boost/operators.hpp>
+
+struct vector_t :
+	boost::addable<vector_t>,
+	boost::subtractable<vector_t>,
+	boost::dividable2<vector_t, float>,
+	boost::multipliable2<vector_t, float>
+{
+	vector_t(){}
+	vector_t(float _x, float _y, float _z);
+
+	const vector_t& operator += (const vector_t &other);
+	const vector_t& operator -= (const vector_t &other);
+	const vector_t& operator /= (const float& other);
+	const vector_t& operator *= (const float& other);
+
 	float x;
 	float y;
 	float z;
 };
 
 typedef vector_t angular_velocity_t;
+
+#include "types.inl"
 
 #endif /* TYPES_H_ */
