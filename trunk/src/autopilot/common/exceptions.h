c9bbdc8251307999c325925019c4787b80453110
yotam.gi <yotam.gi@2154cc09-3a30-70b6-dd6e-86fc0e90be83>
2011-08-31 19:22:19 +0000

    Moved the watch presenter to be a project of it's own, which contains everything, from the device to the watch.

--- trunk/src/autopilot/common/exceptions.h
+++ trunk/src/autopilot/common/exceptions.h
@@ -9,14 +9,4 @@ public:
 	HwExcetion(std::string _what): runtime_error(_what) {}
 };
 
-/**
- * Thrown when a ground station or any other units does not wait for
- * connection.
- */
-class HostException : public std::runtime_error {
-public:
-	HostException(std::string _what): runtime_error(_what) {}
-};
-
-
 #endif /* EXCEPTIONS_H_ */
