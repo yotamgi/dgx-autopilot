2acb1e29a28599ec3a973f3b2dbcd7e081648ba4
yotam.gi <yotam.gi@2154cc09-3a30-70b6-dd6e-86fc0e90be83>
2011-09-03 15:10:23 +0000

    Several things:  - Added the orientation_test to the makefile.  - Fixed bug in the oreintation watch test.wq

--- projs/autopilot/sandbox/orientation_watch_test.cc
+++ projs/autopilot/sandbox/orientation_watch_test.cc
@@ -7,15 +7,15 @@ typedef Watch<float,3> VecWatch;
 int main(int argc, char** argv) {
 
 	Itg3200Gyro gen(2);
-	IntergralFilter<float,3> integ(&gen, -360.0, 360.0f);
-	VecWatch watch(&integ, std::string("192.168.0.19"),
+	IntergralFilter<float,3> integ(&gen, 360.0f, -360.0f);
+	VecWatch watch(&integ, std::string("192.168.0.12"),
 			std::string("gyro_test"), -360.0f, 360.0f);
 
 	watch.run();
 
 	while (true) {
-		watch.get_data();
-		usleep(10000);
+		VecWatch::vector_t v =  watch.get_data();
+		usleep(100);
 	}
 	return 0;
 }
