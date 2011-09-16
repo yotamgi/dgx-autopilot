2f5ac267b6b296d201284e74ec76c80310ca90d6
yotam.gi <yotam.gi@2154cc09-3a30-70b6-dd6e-86fc0e90be83>
2011-09-16 22:33:05 +0000

    Now the static filter compiles and the test are ok.

--- projs/libdata_stream/include/static_filter.h
+++ projs/libdata_stream/include/static_filter.h
@@ -7,15 +7,17 @@ template <class T, size_t N>
 class StaticFilter : public VecFilter<T,N> {
 public:
 
-	StaticFilter(VecGen<T,N>* gen):
-		VecFilter<T,N>(gen),
-		m_add((typename VecFilter<T,N>::vector_t){0., 0., 0.}),
-		m_mul((typename VecFilter<T,N>::vector_t){1., 1., 1.})
-	{}
+	StaticFilter(VecGenerator<T,N>* gen):
+		VecFilter<T,N>(gen)
+	{
+		m_add[0] = 0.; m_add[1] = 0.; m_add[2]=0.;
+		m_mul[0] = 0.; m_mul[1] = 0.; m_mul[2]=0.;
+	}
 
-	StaticFilter(
+	StaticFilter(VecGenerator<T,N>* gen,
 			const typename VecFilter<T,N>::vector_t& add,
 			const typename VecFilter<T,N>::vector_t& mul):
+		VecFilter<T,N>(gen),
 		m_add(add),
 		m_mul(mul)
 	{}
@@ -24,10 +26,10 @@ public:
 		typename VecFilter<T,N>::vector_t data = VecFilter<T,N>::m_generator->get_data();
 
 		for (size_t i = 0; i < N; ++i) {
-			data[i] * m_mul[i];
+			data[i] *= m_mul[i] + m_add[0];
 		}
 
-		return data+m_add;
+		return data;
 	}
 
 private:
