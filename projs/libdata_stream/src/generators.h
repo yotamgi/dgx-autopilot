461cbc1c5ff8f6dcadc4d5c5ada5e27f8c2481a1
yotam.gi <yotam.gi@2154cc09-3a30-70b6-dd6e-86fc0e90be83>
2011-09-18 19:02:29 +0000

    Added src directory in the libdata_stream, and puted all the source there.

---
+++ projs/libdata_stream/src/generators.h
@@ -0,0 +1,89 @@
+#ifndef GANERATOR_H_
+#define GANERATOR_H_
+
+#include <boost/numeric/ublas/vector.hpp>
+#include <boost/numeric/ublas/io.hpp>
+#include <boost/array.hpp>
+
+/**
+ * Represents a any type of data generator. It may be a sensor, or a combination
+ * of some.
+ */
+template <typename data_t>
+class DataGenerator {
+public:
+
+	virtual ~DataGenerator() {}
+
+	/**
+	 * Suppose to return the current value/sample of the data.
+	 */
+	virtual data_t get_data() = 0;
+
+	/**
+	 * Returns true if there was data losed in the generator due to insufficient
+	 * reading speed.
+	 * Should turn false after reading from it.
+	 */
+	virtual bool is_data_losed() = 0;
+
+};
+
+/**
+ * A filter, which is a decoretor to the DataGenerator class.
+ * To implement it, one must implement the get_data function.
+ */
+template <typename data_t>
+class DataFilter : public DataGenerator<data_t> {
+public:
+
+	/**
+	 * Inits the filter with the DataGenerator object it will filter
+	 */
+	DataFilter(DataGenerator<data_t>* generator):m_generator(generator) {}
+
+	/**
+	 * passes the question to the generator, because there is no way how data can
+	 * be lossed in a filter.
+	 */
+	bool is_data_losed() { return m_generator->is_data_losed(); }
+
+protected:
+
+	/**
+	 * The generator that it filters.
+	 */
+	DataGenerator<data_t>* m_generator;
+};
+
+/**
+ * Specific Vector type Data Generator.
+ * It defines the typedef vector_t which is the type of the vector it generates.
+ */
+template <typename T, size_t N>
+class VecGenerator : public DataGenerator< boost::numeric::ublas::vector<T, boost::array<T,N> > >
+{
+public:
+	typedef boost::numeric::ublas::vector<T, boost::array<T,N> > vector_t;
+};
+
+/**
+ * Specific filter for VecGenerators.
+ */
+template <typename T, size_t N>
+class VecFilter : public VecGenerator< T,N >
+{
+public:
+	typedef typename VecGenerator< T,N >::vector_t vector_t;
+
+	VecFilter(VecGenerator<T,N>* generator):m_generator(generator) {}
+
+	bool is_data_losed() { return m_generator->is_data_losed(); }
+
+protected:
+
+	VecGenerator<T,N>* m_generator;
+};
+
+
+#endif /* GANERATOR_H_ */
