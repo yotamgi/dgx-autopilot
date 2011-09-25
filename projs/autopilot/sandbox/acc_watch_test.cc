9daba14abb32d950007531dfcf508fa2de5aa815
yotam.gi <yotam.gi@2154cc09-3a30-70b6-dd6e-86fc0e90be83>
2011-09-25 18:30:19 +0000

    Adopt autopilot to the changes in the watch_test.

--- projs/autopilot/sandbox/acc_watch_test.cc
+++ projs/autopilot/sandbox/acc_watch_test.cc
@@ -1,34 +1,18 @@
-#include "stream_watch.h"
 #include <stream/filters/euler_angles_integral.h>
 #include <stream/filters/static_filter.h>
 #include <stream/util/time.h>
+#include <stream/stream_exporter.h>
 #include "hw/adxl345_acc.h"
 
-typedef Watch<float,3> VecWatch;
-typedef typename Watch<float,3>::vector_t vector_t;
 
 int main(int argc, char** argv) {
-	if (argc != 2) {
-		std::cout << "usage: " << argv[0] << " <server_ip> " << std::endl;	
-		exit(1);
-	}
 	Adxl345Acc gen(2);
-	VecWatch watch(&gen, std::string(argv[1]),
-			std::string("acc_test"), -360.0f, 360.0f);
 
-	watch.run();
+	stream::StreamExporter exporter;
+	exporter.register_stream(&gen, std::string("acc_test"));
 
-	size_t times=0;
-	Timer t;
-	while (true) {
-		VecWatch::vector_t v =  watch.get_data();
-		times ++;
-		if (t.passed() > 1.0) {
-			t.reset();
-			std::cout << "FPS " << times << std::endl;
-			times = 0;
-		}
-		//std::cout << v << std::endl;
-	}
+	std::cout << "Exported stream. waiting for importers..." << std::endl;
+	exporter.run();
+
 	return 0;
 }
--- projs/autopilot/sandbox/gyro_watch_test.cc
+++ projs/autopilot/sandbox/gyro_watch_test.cc
@@ -1,11 +1,10 @@
-#include "stream_watch.h"
+#include "stream/stream_exporter.h"
 #include <stream/filters/euler_angles_integral.h>
 #include <stream/filters/static_filter.h>
 #include <stream/util/time.h>
 #include "hw/itg3200_gyro.h"
 
-typedef Watch<float,3> VecWatch;
-typedef typename Watch<float,3>::vector_t vector_t;
+typedef typename stream::VecGenerator<float,3>::vector_t vector_t;
 
 vector_t calibrate_(stream::VecGenerator<float,3>* gen) {
 	vector_t sum;
@@ -38,21 +37,11 @@ int main(int argc, char** argv) {
 	Itg3200Gyro gen(2);
 	stream::filters::StaticFilter<float,3> s(&gen, a, b);
 	stream::filters::EulerAnglesIntegral<float> integ(&s);
-	VecWatch watch(&integ, std::string(argv[1]),
-			std::string("gyro_test"), -360.0f, 360.0f);
 
-	watch.run();
+	stream::StreamExporter exporter;
+	exporter.register_stream(&integ, std::string("gyro_test"));
 
-	size_t times;
-	Timer t;
-	while (true) {
-		VecWatch::vector_t v =  watch.get_data();
-		times ++;
-		if (t.passed() > 1.0) {
-			t.reset();
-			std::cout << "FPS " << times << std::endl;
-			times = 0;
-		}
-	}
+	exporter.run();
+
 	return 0;
 }
