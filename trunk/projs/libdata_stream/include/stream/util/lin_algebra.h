ec685ac5fa140fc41cec282b03f1fdc6fe8dee53
yotam.gi <yotam.gi@2154cc09-3a30-70b6-dd6e-86fc0e90be83>
2011-09-29 22:01:14 +0000

    Added the lin_algebra lib and the euler angles integral uses it.

---
+++ trunk/projs/libdata_stream/include/stream/util/lin_algebra.h
@@ -0,0 +1,59 @@
+/*
+ * lin_algebra.h
+ *
+ *  Created on: Sep 30, 2011
+ *      Author: yotam
+ */
+
+#ifndef LIN_ALGEBRA_H_
+#define LIN_ALGEBRA_H_
+
+#include <boost/numeric/ublas/matrix.hpp>
+#include <boost/numeric/ublas/matrix_proxy.hpp>
+#include <boost/numeric/ublas/io.hpp>
+
+
+namespace lin_algebra {
+
+using namespace boost::numeric::ublas;
+
+/////////////////////////////////////////////////////////////////////
+// Types
+//////////////////////////
+typedef boost::numeric::ublas::vector<float_t> vector_t;
+typedef boost::numeric::ublas::matrix<float_t> matrix_t;
+
+/////////////////////////////////////////////////////////////////////
+// Constants
+//////////////////////////
+const float_t PI = 3.14159265;
+
+
+/////////////////////////////////////////////////////////////////////
+// Methods
+//////////////////////////
+typedef boost::numeric::ublas::matrix_row<matrix_t> mat_row;
+typedef boost::numeric::ublas::matrix_column<matrix_t> mat_col;
+
+void orthogonalize(vector_t& a, vector_t& b) {
+	a[2] = -1.0 * (a[0]*b[0] + a[1]*b[1]) / b[2];
+}
+
+/**
+ * Expects two normalized vectors
+ */
+float angle_between(const vector_t& a, const vector_t& b) {
+
+	return std::acos(a[0]*b[0] + a[1]*b[1] + a[2]*b[2]) * 180. / PI;
+}
+
+float vec_len(const vector_t& vec) {
+	return std::sqrt(vec[0]*vec[0] + vec[1]*vec[1] + vec[2]*vec[2]);
+}
+
+void normalize(vector_t& vec) {
+	vec /= vec_len(vec);
+}
+}
+
+#endif /* LIN_ALGEBRA_H_ */
