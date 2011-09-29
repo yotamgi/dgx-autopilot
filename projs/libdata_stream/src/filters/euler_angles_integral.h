7db690101ca2a68429801c8b7e3423dc6c9d2446
yotam.gi <yotam.gi@2154cc09-3a30-70b6-dd6e-86fc0e90be83>
2011-09-29 22:01:14 +0000

    Added the lin_algebra lib and the euler angles integral uses it.

--- projs/libdata_stream/src/filters/euler_angles_integral.h
+++ projs/libdata_stream/src/filters/euler_angles_integral.h
@@ -6,15 +6,15 @@
 #include <cmath>
 
 #include <boost/array.hpp>
-#include <boost/numeric/ublas/matrix.hpp>
-#include <boost/numeric/ublas/io.hpp>
-
 #include "stream/generators.h"
 #include "stream/data_filter.h"
 #include "stream/util/time.h"
+#include "stream/util/lin_algebra.h"
 
 namespace stream {
 namespace filters {
+
+using namespace lin_algebra;
 
 /*
  * TODO:
@@ -24,14 +24,11 @@ template <typename float_t>
 class EulerAnglesIntegral : public VecFilter<float_t,3> {
 public:
 
-	typedef typename boost::numeric::ublas::vector<float_t> vector_t;
 
 	EulerAnglesIntegral(VecGenerator<float_t,3>* data_gen):
 		VecFilter<float_t,3>(data_gen),
 		m_sum((vector_t[3]){ vector_t(3), vector_t(3),vector_t(3) }	),
-		rot_sum(0.),
-		m_prev_time(get_curr_time()),
-		PI(3.141592654)
+		m_prev_time(get_curr_time())
 	{
 		// initialize the coordinate system
 		m_sum[0][0] = 1.; m_sum[0][1] =  0.; m_sum[0][2] =  0.;
@@ -51,27 +48,28 @@ public:
 		double time_delta = curr_time - m_prev_time;
 		m_prev_time = curr_time;
 
-		rot_sum += data[2] * time_delta;
-
 		float_t wx = data[0]*time_delta / 180. * PI;
 		float_t wy = data[1]*time_delta / 180. * PI;
 		float_t wz = data[2]*time_delta / 180. * PI;
 
-		boost::numeric::ublas::matrix<float> update(3, 3);
+		matrix_t update(3, 3);
 		update(0,0) = 0.;		update(0,1) = -1.*wz;	update(0,2) = wy;
 		update(1,0) = wz;   	update(1,1) = 0.;   	update(1,2) = -1.*wx;
 		update(2,0) = -1.*wy;  	update(2,1) = wx;   	update(2,2) = 0.;
 
-		boost::numeric::ublas::matrix<float> curr(3, 3);
+		matrix_t curr(3, 3);
 		curr(0,0) = m_sum[0][0];	curr(0,1) = m_sum[0][1];	curr(0,2) = m_sum[0][2];
 		curr(1,0) = m_sum[1][0];   	curr(1,1) = m_sum[1][1];   	curr(1,2) = m_sum[1][2];
 		curr(2,0) = m_sum[2][0];  	curr(2,1) = m_sum[2][1];   	curr(2,2) = m_sum[2][2];
 
 		curr = curr +  boost::numeric::ublas::prod(update, curr);
 
-		m_sum[0][0] = curr(0,0);	m_sum[0][1] = curr(0,1);	m_sum[0][2] = curr(0,2);
-		m_sum[1][0] = curr(1,0);   	m_sum[1][1] = curr(1,1);   	m_sum[1][2] = curr(1,2);
-		m_sum[2][0] = curr(2,0);  	m_sum[2][1] = curr(2,1);   	m_sum[2][2] = curr(2,2);
+//		m_sum[0][0] = curr(0,0);	m_sum[0][1] = curr(0,1);	m_sum[0][2] = curr(0,2);
+//		m_sum[1][0] = curr(1,0);   	m_sum[1][1] = curr(1,1);   	m_sum[1][2] = curr(1,2);
+//		m_sum[2][0] = curr(2,0);  	m_sum[2][1] = curr(2,1);   	m_sum[2][2] = curr(2,2);
+		m_sum[0] = mat_row(curr, 0);
+		m_sum[1] = mat_row(curr, 1);
+		m_sum[2] = mat_row(curr, 2);
 
 		// maintain the coordinate orthogonal
 		orthogonalize(m_sum[0], m_sum[1]);
@@ -134,28 +132,8 @@ private:
 		return ans;
 	}
 
-	void normalize(vector_t& vec) {
-		vec /= vec_len(vec);
-	}
-
 	float_t sign(float_t num) {
 		return (num>0.)?1.:-1.;
-	}
-
-	void orthogonalize(vector_t& a, vector_t& b) {
-		a[2] = -1.0 * (a[0]*b[0] + a[1]*b[1]) / b[2];
-	}
-
-	/**
-	 * Expects two normalized vectors
-	 */
-	float angle_between(const vector_t& a, const vector_t& b) const {
-
-		return std::acos(a[0]*b[0] + a[1]*b[1] + a[2]*b[2]) * 180. / PI;
-	}
-
-	float_t vec_len(const vector_t& vec) const {
-		return std::sqrt(vec[0]*vec[0] + vec[1]*vec[1] + vec[2]*vec[2]);
 	}
 
 	/**
@@ -170,8 +148,6 @@ private:
 	 * Needed for time_delta calculation.
 	 */
 	double m_prev_time;
-
-	const float_t PI;
 };
 
 } // namespace filters
