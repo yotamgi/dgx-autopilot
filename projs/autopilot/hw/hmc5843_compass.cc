30fe56bbfea05c055677e804d030c9dbd2cca9b3
yotam.gi <yotam.gi@2154cc09-3a30-70b6-dd6e-86fc0e90be83>
2011-10-01 17:45:46 +0000

    Added the compass generator.

---
+++ projs/autopilot/hw/hmc5843_compass.cc
@@ -0,0 +1,47 @@
+#include "hmc5843_compass.h"
+#include <stdint.h>
+#include <sstream>
+
+Hmc5843Compass::Hmc5843Compass(size_t device_i2c_num):
+			I2C_ADDRESS(0x1e),
+			WHO_AM_I_VALUE(0x2),
+			CONFIGURATION_REGISTER_A(0x00),
+			CONFIGURATION_REGISTER_B(0x01),
+			MODE_REGISTER(0x02),
+			X_READ_ADDRES(0x03),
+			Y_READ_ADDRES(0x05),
+			Z_READ_ADDRES(0x07),
+			WHO_AM_I_1(0x10),
+			m_i2c(device_i2c_num, I2C_ADDRESS)
+{
+	// sanity check
+	uint8_t who_am_i = m_i2c.read_num<uint8_t>(WHO_AM_I_1);
+	if (who_am_i != WHO_AM_I_VALUE) {
+		std::stringstream ss;
+		ss << "The value of the who am i value of the hmc5843 compass was " <<
+				std::hex << (int)who_am_i << "and not " << (int)WHO_AM_I_VALUE;
+		throw HwExcetion(ss.str());
+	}
+
+	// configuration
+	m_i2c.write_num<uint8_t>(CONFIGURATION_REGISTER_A, 0x6 << 2); // 50 tims a second
+	m_i2c.write_num<uint8_t>(MODE_REGISTER, 0);
+}
+
+Hmc5843Compass::vector_t Hmc5843Compass::get_data() {
+
+	// read the data
+	vector_t fans;
+	fans[0] = (float)m_i2c.read_num<uint16_t>(X_READ_ADDRES);
+	fans[1] = (float)m_i2c.read_num<uint16_t>(Y_READ_ADDRES);
+	fans[2] = (float)m_i2c.read_num<uint16_t>(Z_READ_ADDRES);
+	
+	//std::cout << ans[0] << std::endl;
+	//std::cout << (ans[0] << 8) + (ans[0] >> 8) << std::endl;
+
+	//fans[0] = float((int16_t)((ans[0]<<8)&0xff00) + (ans[0]>>8));
+	//fans[1] = float((int16_t)((ans[1]<<8)&0xff00) + (ans[1]>>8));
+	//fans[2] = float((int16_t)((ans[2]<<8)&0xff00) + (ans[2]>>8));
+	//std::cout << fans << std::endl;
+	return fans;
+}
