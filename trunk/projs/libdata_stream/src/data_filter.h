6834f39286cf1b2e4b77e61cddabbf90085d2ed7
yotam.gi <yotam.gi@2154cc09-3a30-70b6-dd6e-86fc0e90be83>
2011-10-05 18:57:51 +0000

    Added support for filters to change their type.

--- trunk/projs/libdata_stream/src/data_filter.h
+++ trunk/projs/libdata_stream/src/data_filter.h
@@ -10,46 +10,37 @@ namespace stream {
  * A filter, which is a decoretor to the DataGenerator class.
  * To implement it, one must implement the get_data function.
  */
-template <typename data_t>
+template <typename data_t, typename data_source_t = data_t>
 class DataFilter : public DataGenerator<data_t> {
 public:
 
 	/**
 	 * Inits the filter with the DataGenerator object it will filter
 	 */
-	DataFilter(DataGenerator<data_t>* generator):m_generator(generator) {}
-
-	/**
-	 * passes the question to the generator, because there is no way how data can
-	 * be lossed in a filter.
-	 */
-	bool is_data_losed() { return m_generator->is_data_losed(); }
+	DataFilter(DataGenerator<data_source_t>* generator):m_generator(generator) {}
 
 protected:
 
 	/**
 	 * The generator that it filters.
 	 */
-	DataGenerator<data_t>* m_generator;
+	DataGenerator<data_source_t>* m_generator;
 };
-
 
 /**
  * Specific filter for VecGenerators.
  */
-template <typename T, size_t N>
+template <typename T, size_t N, typename SourceT=T, size_t SourceN=N>
 class VecFilter : public VecGenerator< T,N >
 {
 public:
 	typedef typename VecGenerator< T,N >::vector_t vector_t;
 
-	VecFilter(VecGenerator<T,N>* generator):m_generator(generator) {}
-
-	bool is_data_losed() { return m_generator->is_data_losed(); }
+	VecFilter(VecGenerator<SourceT,SourceN>* generator):m_generator(generator) {}
 
 protected:
 
-	VecGenerator<T,N>* m_generator;
+	VecGenerator<SourceT,SourceN>* m_generator;
 };
 
 } // namespace stream
