ba40280a552b174e3e1426b9b8e0fcdb78ba1cf1
yotam.gi <yotam.gi@2154cc09-3a30-70b6-dd6e-86fc0e90be83>
2011-10-14 17:46:28 +0000

    Added the vectorized version of the low_pass_filter.

--- projs/libdata_stream/src/filters/low_pass_filter.h
+++ projs/libdata_stream/src/filters/low_pass_filter.h
@@ -26,6 +26,25 @@ private:
 	size_t m_left_to_reset;
 };
 
+template <typename T, size_t N>
+class LowPassVecFilter : public VecFilter<T,N> {
+public:
+	LowPassVecFilter(boost::shared_ptr<VecGenerator<T,N> > vec_gen, size_t filt);
+
+	typename VecFilter<T,N>::vector_t get_data();
+
+private:
+
+	typename VecFilter<T,N>::vector_t m_sum;
+	boost::circular_buffer<typename VecFilter<T,N>::vector_t> m_samples;
+	const size_t m_size;
+
+	/* Variables for the sum resetting */
+	const size_t m_max;
+	size_t m_left_to_reset;
+};
+
+
 }  // namespace filter
 }  // namespace stream
 
