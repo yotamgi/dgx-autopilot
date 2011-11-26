ac4c04ceaa9a460a24df0164516a6cb520936059
yotam.gi <yotam.gi@2154cc09-3a30-70b6-dd6e-86fc0e90be83>
2011-11-26 10:24:36 +0000

    Now realy finished the refactor.

--- projs/libdata_stream/src/data_filter.h
+++ projs/libdata_stream/src/data_filter.h
@@ -35,13 +35,13 @@ protected:
  * To implement it, one must implement the get_data function.
  */
 template <typename data_source_t, typename data_t = data_source_t>
-class StreamPushFilter : public DataCollector<data_t> {
+class StreamPushFilter : public DataPushStream<data_t> {
 public:
 
 	/**
 	 * Inits the filter with the DataPopStream object it will filter
 	 */
-	StreamPushFilter(boost::shared_ptr<DataCollector<data_source_t> > collector):m_collector(collector) {}
+	StreamPushFilter(boost::shared_ptr<DataPushStream<data_source_t> > collector):m_collector(collector) {}
 
 protected:
 
@@ -55,7 +55,7 @@ protected:
  * The Generic filter class - for any kind of stream
  */
 template <typename data_source_t, typename data_t = data_source_t>
-class StreamFilter : public StreamPopFilter<data_t>, public DataCollector<data_t> {};
+class StreamFilter : public StreamPopFilter<data_t>, public DataPushStream<data_t> {};
 
 
 
