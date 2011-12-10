e2983410596a9e9403b8a3c19c7814e0473548bc
yotam.gi <yotam.gi@2154cc09-3a30-70b6-dd6e-86fc0e90be83>
2011-12-10 22:16:52 +0000

    Fixed some things in the waypoint pilot. now the pitch works, but the tilt still doesn't

--- projs/autopilot/src/waypoint_pilot.cc
+++ projs/autopilot/src/waypoint_pilot.cc
@@ -34,28 +34,33 @@ void WaypointPilot::fly() {
 		// calculate the planes position, altitude and orientation
 		lin_algebra::vec3f plain_orientation = oreintation->get_data();
 		lin_algebra::vec3f plain_pos_and_alt = position->get_data();
+		lin_algebra::vec3f plain_speed = speed->get_data();
 		lin_algebra::vec2f plain_pos;
 		plain_pos[0] = plain_pos_and_alt[0]; plain_pos[1] = plain_pos_and_alt[2];
+		lin_algebra::vec2f plain_direction;
+		plain_direction[0] = plain_speed[0]; plain_direction[1] = plain_speed[2];
 		float plain_alt = plain_pos_and_alt[1];
 
 		// understand the wanted direction and altitude
-		lin_algebra::vec2f wanted_diretion = m_target - lin_algebra::normalize(plain_pos);
+		lin_algebra::vec2f wanted_diretion = m_target - plain_pos;
+
 		float delta_alt = m_altitude - plain_alt;
-
 		float expected_pitch;
-		if (delta_alt >  20.) 		expected_pitch = std::sin(m_params.avg_climbing_angle);
-		else if (delta_alt < -20.) 	expected_pitch = std::sin(m_params.avg_climbing_angle);
+		if (delta_alt >  20.) 		expected_pitch =  std::sin(m_params.avg_climbing_angle);
+		else if (delta_alt < -20.) 	expected_pitch = -std::sin(m_params.avg_climbing_angle);
 		else expected_pitch = delta_alt/20. / 4.;
-		expected_pitch = std::max(expected_pitch,  std::sin(m_params.max_climbing_angle));
-		expected_pitch = std::min(expected_pitch, -std::sin(m_params.max_decending_angle));
-
-		float expected_tilt = std::tan(wanted_diretion[1]/wanted_diretion[0])/lin_algebra::PI;
-		expected_tilt = std::max(expected_tilt,  std::sin(m_params.max_tilt_angle));
-		expected_tilt = std::min(expected_tilt, -std::sin(m_params.max_tilt_angle));
+		expected_pitch = std::min(expected_pitch,  std::sin(m_params.max_climbing_angle));
+		expected_pitch = std::max(expected_pitch, -std::sin(m_params.max_decending_angle));
 
 		// control the stick to achieve the speed vector we calculated
-		float pitch_data = plain_orientation[0]/180. - expected_pitch;
-		float tilt_data  = plain_orientation[2]/180. - expected_tilt;
+		lin_algebra::vec2f delta_dir = wanted_diretion - plain_direction;
+		delta_dir = lin_algebra::normalize(delta_dir);
+		float expected_tilt = std::atan(delta_dir[1]/delta_dir[0])/lin_algebra::PI;
+		expected_tilt = std::min(expected_tilt,  std::sin(m_params.max_tilt_angle));
+		expected_tilt = std::max(expected_tilt, -std::sin(m_params.max_tilt_angle));
+
+		float pitch_data = expected_pitch - plain_orientation[0]/180.;
+		float tilt_data  = expected_tilt  - plain_orientation[2]/180.;
 		m_cockpit->pitch_servo()->set_data(50. + 50. * pitch_data);
 		m_cockpit->tilt_servo()->set_data(50. + 50. * tilt_data);
 	}
