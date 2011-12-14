bc1c2f6297e52e6489a99c30ab315cc111d3d762
yotam.gi <yotam.gi@2154cc09-3a30-70b6-dd6e-86fc0e90be83>
2011-12-14 22:10:21 +0000

    Now the gps filter uses the principal analasys armadillo function.

--- trunk/projs/autopilot/src/gps_filter.cc
+++ trunk/projs/autopilot/src/gps_filter.cc
@@ -20,22 +20,21 @@ SimpleGpsFilter::SimpleGpsFilter(size_t avarge_len):
 void SimpleGpsFilter::set_data(const lin_algebra::vec3f& data) {
 	m_samples.push_back(data);
 
+	// fill the data into a matrix
+	lin_algebra::Mat<float> m(3, m_samples.size());
+	for (size_t i=0; i<m_samples.size(); i++) {
+		m.col(i) = m_samples.at(i);
+	}
+	m = lin_algebra::trans(m);
+
+	lin_algebra::Mat<float> ce, score;
+	lin_algebra::princomp(ce, score, m);
+	lin_algebra::vec3f speed = ce.col(0) * score.at(0,0) * -2.;
+	float reliability = score.at(0, 1);
+
+	m_speed_stream->set_data(speed);
 	m_position_stream->set_data(data);
-	m_speed_stream->set_data(data - m_samples.front());
-
-//	// fill the data into a matrix
-//	lin_algebra::Mat<float> m(3, m_samples.size());
-//	for (size_t i=0; i<m_samples.size(); i++) {
-//		m.col(i) = m_samples.at(i);
-//	}
-//	m = lin_algebra::trans(m);
-//
-//	lin_algebra::Mat<float> ce;
-//	lin_algebra::princomp(ce, m);
-//	std::cout << ce << std::endl;
-//
-//	lin_algebra::vec3f mean = lin_algebra::trans(lin_algebra::mean(m));
-
+	m_speed_reliable_stream->set_data(reliability);
 }
 
 boost::shared_ptr<stream::DataPopStream<lin_algebra::vec3f> > SimpleGpsFilter::get_position_stream() {
@@ -44,4 +43,8 @@ boost::shared_ptr<stream::DataPopStream<lin_algebra::vec3f> > SimpleGpsFilter::g
 
 boost::shared_ptr<stream::DataPopStream<lin_algebra::vec3f> > SimpleGpsFilter::get_speed_stream() {
 	return m_speed_stream;
+}
+
+boost::shared_ptr<stream::DataPopStream<float> > SimpleGpsFilter::get_speed_reliable_stream() {
+	return m_speed_reliable_stream;
 }
