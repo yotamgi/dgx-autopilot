2f66bca5585d1955a769a41c80acb1738d0a2e51
yotam.gi <yotam.gi@2154cc09-3a30-70b6-dd6e-86fc0e90be83>
2011-12-22 12:26:50 +0000

    First real version of the MapStreamView widget!

--- projs/ground_station/src/map_stream_view.h
+++ projs/ground_station/src/map_stream_view.h
@@ -1,18 +1,40 @@
 #ifndef MAPSTREAMVIEW_H_
 #define MAPSTREAMVIEW_H_
 
+#include <stream/data_pop_stream.h>
+#include <stream/util/lin_algebra.h>
+#include <boost/shared_ptr.hpp>
+#include <boost/thread.hpp>
 #include <QtGui>
 #include <qgis/qgsmapcanvas.h>
 
 namespace gs {
 
+typedef stream::DataPopStream<lin_algebra::vec2f> pos_stream;
+
 class MapStreamView : public QWidget {
+	Q_OBJECT
 public:
 
-	MapStreamView(std::string map_fname, std::string map_type);
+	MapStreamView(std::string map_fname,
+				  std::string map_type,
+				  boost::shared_ptr<pos_stream> pos_stream,
+				  float update_time);
+
+public slots:
+
+	void update();
 
 private:
+
 	QgsMapCanvas* m_map_canvas;
+	boost::shared_ptr<QgsRubberBand> m_plane_track;
+
+	boost::shared_ptr<pos_stream> m_pos_stream;
+
+	float m_update_time;
+
+	QTimer* m_timer;
 };
 
 }; // namespace gs
