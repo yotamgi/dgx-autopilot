4c2fe95ce427fe32c341ecd0144c4d2f10b36e33
yotam.gi <yotam.gi@2154cc09-3a30-70b6-dd6e-86fc0e90be83>
2011-12-22 12:26:50 +0000

    First real version of the MapStreamView widget!

--- trunk/projs/ground_station/src/main.cc
+++ trunk/projs/ground_station/src/main.cc
@@ -1,14 +1,34 @@
 #include "map_stream_view.h"
 #include <QtGui>
 
+class SimplePosStream : public stream::DataPopStream<lin_algebra::vec2f> {
+public:
+	SimplePosStream(): m_num(0.) {}
+
+	lin_algebra::vec2f get_data()  {
+
+		lin_algebra::vec2f pos;
+		pos[0] = m_num;
+		pos[1] = m_num/5.;
+		m_num -= 1.0;
+		return pos;
+	}
+
+private:
+	float m_num;
+};
+
+
 int main(int argc, char** argv) {
+	boost::shared_ptr<SimplePosStream> pos_stream(new SimplePosStream);
 	QApplication app(argc, argv);
 
 	QVBoxLayout* layout = new QVBoxLayout();
 	layout->addWidget(new QPushButton("&A"));
 	layout->addWidget(new QPushButton("&B"));
 
-	layout->addWidget(new gs::MapStreamView("./data/map", "ogr"));
+	gs::MapStreamView* map_view = new gs::MapStreamView("./data/map", "ogr", pos_stream, 0.2);
+	layout->addWidget(map_view);
 
 	QWidget* window = new QWidget();
 	window->setLayout(layout);
--- trunk/projs/ground_station/src/map_stream_view.cc
+++ trunk/projs/ground_station/src/map_stream_view.cc
@@ -3,24 +3,32 @@
 #include <qgis/qgsvectorlayer.h>
 #include <qgis/qgssinglesymbolrenderer.h>
 #include <qgis/qgsmaplayerregistry.h>
+#include <qgis/qgsrubberband.h>
 #include <stdexcept>
 
 namespace gs {
 
-MapStreamView::MapStreamView(std::string map_fname, std::string map_type):
-		m_map_canvas(new QgsMapCanvas(0,0))
+MapStreamView::MapStreamView(std::string map_fname,
+		  	  	  	  	  	 std::string map_type,
+		  	  	  	  	  	 boost::shared_ptr<pos_stream> pos_stream,
+		  	  	  	  	  	 float update_time):
+		  	 m_map_canvas(new QgsMapCanvas(0,0)),
+		  	 m_pos_stream(pos_stream),
+		  	 m_update_time(update_time)
 {
 	QgsProviderRegistry::instance("/usr/lib/qgis");
 
 	// create and configure the map canvas
-	m_map_canvas = new QgsMapCanvas(0, 0);
+	m_map_canvas = new QgsMapCanvas();
 	m_map_canvas->enableAntiAliasing(true);
 	m_map_canvas->useImageToRender(false);
 	m_map_canvas->setCanvasColor(QColor(255, 255, 255));
 	m_map_canvas->freeze(false);
 	m_map_canvas->setVisible(true);
 	m_map_canvas->refresh();
+	m_map_canvas->setFixedSize(QSize(300, 200));
 	m_map_canvas->show();
+
 
 	// create the vector layer
 	QgsVectorLayer * map_layer = new QgsVectorLayer(QString(map_fname.c_str()), tr("layer1"), QString(map_type.c_str()));
@@ -37,13 +45,32 @@ MapStreamView::MapStreamView(std::string map_fname, std::string map_type):
 	m_map_canvas->setExtent(map_layer->extent());
 	m_map_canvas->setLayerSet(map_layer_set);
 
-	QgsPoint p(0., 0.);
-	m_map_canvas->setExtent(QgsRectangle(p, p));
-	m_map_canvas->refresh();
+
+ 	m_plane_track = boost::shared_ptr<QgsRubberBand>(new QgsRubberBand(m_map_canvas, false));
+	m_map_canvas->scene()->addItem(m_plane_track.get());
 
 	QVBoxLayout* layout = new QVBoxLayout();
 	layout->addWidget(m_map_canvas);
 	setLayout(layout);
+
+ 	m_timer = new QTimer(this);
+	connect(m_timer, SIGNAL(timeout()), this, SLOT(update()));
+	m_timer->start(1000*m_update_time);
+}
+
+void MapStreamView::update() {
+	lin_algebra::vec2f pos = m_pos_stream->get_data();
+	QgsPoint p(pos[0], pos[1]);
+
+	// go to the current plane pos on the map
+	std::cout << "Setting extent" << std::endl;
+	m_map_canvas->setExtent(QgsRectangle(p, p));
+	m_map_canvas->refresh();
+
+	// draw the line of the plane's way
+	std::cout << "adding point" << std::endl;
+	m_plane_track->addPoint(p);
+	m_map_canvas->refresh();
 }
 
 } // namespace gs
