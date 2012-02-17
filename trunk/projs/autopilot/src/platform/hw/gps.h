5c3daab5b313070427e0e9a16881eeb14d1188ae
yotam.gi <yotam.gi@2154cc09-3a30-70b6-dd6e-86fc0e90be83>
2012-02-17 18:27:48 +0000

    Improved the GPS class and added the GPS test.

--- trunk/projs/autopilot/src/platform/hw/gps.h
+++ trunk/projs/autopilot/src/platform/hw/gps.h
@@ -1,26 +1,33 @@
 #ifndef GPS_H_
 #define GPS_H_
 
-#include <libgpsmm.h>
 #include <stream/data_push_stream.h>
+#include <stream/util/lin_algebra.h>
 #include <boost/shared_ptr.hpp>
 #include <boost/thread.hpp>
+#include <libgpsmm.h>
 
 namespace autopilot {
 
-typedef boost::shared_ptr<stream::DataPushStream> reciever_ptr;
+typedef boost::shared_ptr<stream::DataPushStream<float> > 				mag_reciever_ptr;
+typedef boost::shared_ptr<stream::DataPushStream<lin_algebra::vec2f> > 	vec2_reciever_ptr;
 
 class Gps {
+public:
 	Gps();
 
-	void set_reciever_stream(reciever_ptr reciever);
+	void set_pos_reciever_stream(vec2_reciever_ptr reciever);
+	void set_speed_mag_reciever_stream(mag_reciever_ptr reciever);
+	void set_speed_dir_reciever_stream(mag_reciever_ptr reciever);
 
 private:
 
 	void update();
 
 	boost::thread m_update_thread;
-	reciever_ptr m_reciever;
+	mag_reciever_ptr m_speed_mag;
+	mag_reciever_ptr m_speed_dir;
+	vec2_reciever_ptr m_pos;
 	gpsmm m_gpsmm;
 };
 
---
+++ trunk/projs/autopilot/src/sandbox/gps_test.cc
@@ -0,0 +1,31 @@
+#include <iostream>
+#include <string>
+#include <platform/hw/gps.h>
+
+
+template <typename T>
+class PushPrinter : public stream::DataPushStream<T> {
+public:
+	PushPrinter(std::string name): m_name(name) {}
+
+	void set_data(const T& t){
+		std::cout << m_name << ": " << t << std::endl;
+	}
+
+private:
+	std::string m_name;
+};
+
+int main() {
+	boost::shared_ptr<PushPrinter<lin_algebra::vec2f> > pos(new PushPrinter<lin_algebra::vec2f>("Position"));
+	boost::shared_ptr<PushPrinter<float> > dir(new PushPrinter<float>("Direction"));
+	boost::shared_ptr<PushPrinter<float> > speed(new PushPrinter<float>("Speed"));
+
+	autopilot::Gps gps;
+	gps.set_pos_reciever_stream(pos);
+	gps.set_speed_mag_reciever_stream(speed);
+	gps.set_speed_dir_reciever_stream(dir);
+
+	while (true) {}
+	return 0;
+}
