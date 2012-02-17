20ad9e74ab08772218674d0c230f44accf0b3bf4
yotam.gi <yotam.gi@2154cc09-3a30-70b6-dd6e-86fc0e90be83>
2012-02-17 18:27:48 +0000

    Improved the GPS class and added the GPS test.

--- projs/autopilot/src/platform/hw/gps.cc
+++ projs/autopilot/src/platform/hw/gps.cc
@@ -2,28 +2,48 @@
 
 namespace autopilot {
 
-Gps::Gps():m_update_thread(Gps::update)
-{
+Gps::Gps():m_update_thread(&Gps::update, this)
+{}
 
+void Gps::set_pos_reciever_stream(vec2_reciever_ptr reciever) {
+	m_pos = reciever;
+}
+void Gps::set_speed_mag_reciever_stream(mag_reciever_ptr reciever) {
+	m_speed_mag = reciever;
+}
+void Gps::set_speed_dir_reciever_stream(mag_reciever_ptr reciever) {
+	m_speed_dir = reciever;
 }
 
-
-void Gps::set_reciever_stream(reciever_ptr reciever) {
-	m_reciever = reciever;
-}
 
 void Gps::update() {
 	// init
 	m_gpsmm.open();
-	if (mm.stream(WATCH_ENABLE | WATCH_JSON) == NULL) {
-		std::cout << "No GPS!" << std::endl;
+	if (m_gpsmm.stream(WATCH_ENABLE | WATCH_JSON) == NULL) {
+		throw stream::PushStreamException("No GPS Found");
 	}
 
 	while (true) {
-		struct gps_data_t* data = mm.poll();
-		std::cout << data->fix.altitude << std::endl;
-	}
+		struct gps_data_t* data = m_gpsmm.poll();
 
+		if (data->fix.mode >= 2) {
+
+			if (m_pos) {
+				lin_algebra::vec2f pos = lin_algebra::create_vec2f(
+						data->fix.longitude, data->fix.latitude
+				);
+				m_pos->set_data(pos);
+ 			}
+
+			if (m_speed_dir) {
+				m_speed_dir->set_data(data->fix.track);
+			}
+
+			if (m_speed_mag) {
+				m_speed_mag->set_data(data->fix.speed);
+			}
+		}
+	}
 }
 
 }  // namespace autopilot
