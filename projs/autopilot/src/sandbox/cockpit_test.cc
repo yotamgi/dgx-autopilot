1d03f16ecb0f1c70d34247be1695b21f55fa82e8
yotam.gi <yotam.gi@2154cc09-3a30-70b6-dd6e-86fc0e90be83>
2012-02-23 23:07:51 +0000

    + fixed the cockpit_test_device - now it actually compiles. + added the --record and --play to the cockpit_test. It still requires some work in order for it to function.

--- projs/autopilot/src/sandbox/cockpit_test.cc
+++ projs/autopilot/src/sandbox/cockpit_test.cc
@@ -1,5 +1,7 @@
 #include <stream/stream_connection.h>
+#include <stream/filters/stream_recorder.h>
 #include <stream/util/tcpip_connection.h>
+#include <stream/util/stream_player.h>
 #include <platform/dgx1_platform.h>
 #include <cockpit.h>
 #include <gs/3d_stream_view.h>
@@ -8,11 +10,13 @@
 
 #include <string>
 #include <iostream>
+#include <fstream>
 
 typedef stream::filters::WatchFilter<lin_algebra::vec3f> vec3_watch_stream;
 
 void usage(std::string arg0) {
-	std::cout << arg0 << " [--sensor-sim <address> ]  [--present-from <address> ] [--present-local]  [--help]" << std::endl;
+	std::cout << arg0 << " --sensor-sim <address>  [--present-from <address> ]"
+			"[--record <folder> ] [--play <folder> ] [--help]" << std::endl;
 }
 
 void update_cockpit(autopilot::Cockpit* cockpit) {
@@ -28,9 +32,10 @@ int main(int argc, char** argv) {
 
 	// commandline parsing
 	bool sim = false;
+	std::string record_dir;
+	std::string play_dir;
 	std::string sim_addr;
 	std::string present_addr;
-	bool present_local = false;
 	if (argc > 2) {
 		for (size_t i=1; i<(size_t)argc; i++) {
 
@@ -45,8 +50,24 @@ int main(int argc, char** argv) {
 				sim = true;
 				continue;
 			}
-			else if (std::string(argv[i]) == "--present-local") {
-				present_local = true;
+			if (std::string(argv[i]) == "--record") {
+				if ((size_t)argc < i+1) {
+					usage(argv[0]);
+					exit(1);
+				} else {
+					record_dir = argv[i+1];
+					i++;
+				}
+				continue;
+			}
+			if (std::string(argv[i]) == "--play") {
+				if ((size_t)argc < i+1) {
+					usage(argv[0]);
+					exit(1);
+				} else {
+					play_dir = argv[i+1];
+					i++;
+				}
 				continue;
 			}
 			else if (std::string(argv[i]) == "--present-from") {
@@ -70,7 +91,43 @@ int main(int argc, char** argv) {
 		exit(1);
 	}
 
-	if (present_addr != "")  {
+	if (play_dir != "") {
+		typedef stream::PopStreamPlayer<lin_algebra::vec3f> vec3_player;
+
+		std::ifstream acc_file((play_dir + "/acc.stream").c_str());
+		std::ifstream compass_file((play_dir + "/compass.stream").c_str());
+		std::ifstream gyro_file((play_dir + "/gyro.stream").c_str());
+
+		// create the player platform
+		autopilot::NormalPlainPlatform platform;
+		platform.acc_sensor = boost::make_shared<vec3_player>(boost::ref(acc_file));
+		platform.gyro_sensor = boost::make_shared<vec3_player>(boost::ref(gyro_file));
+		platform.compass_sensor = boost::make_shared<vec3_player>(boost::ref(compass_file));
+
+		// add a watch stream on the compass stream
+		boost::shared_ptr<vec3_watch_stream> compass_watch(new vec3_watch_stream(platform.compass_sensor));
+		platform.compass_sensor = compass_watch;
+
+		autopilot::Cockpit cockpit(platform);
+		boost::thread update_thread(update_cockpit, &cockpit);
+		QApplication app(argc, argv);
+		gs::StreamView3d view3d(view_update_time, stream3d_dimention);
+
+		// the left one
+		view3d.addAngleStream(cockpit.orientation()->get_watch_stream(), irr::core::vector3df(-20., 0., 0.));
+
+		// the mid one
+		view3d.addAngleStream(cockpit.watch_rest_orientation(), irr::core::vector3df(0., 0., 0.));
+		view3d.addVecStream(cockpit.watch_fixed_acc(), irr::core::vector3df(0., 0., 0.));
+		view3d.addVecStream(compass_watch->get_watch_stream(), irr::core::vector3df(0., 0., 0.));
+
+		// the right one
+		view3d.addAngleStream(cockpit.watch_gyro_orientation(), irr::core::vector3df(20., 0., 0.));
+
+		// the reliable stream
+		gs::SizeStreamView view_size(cockpit.watch_rest_reliability(), view_update_time, 0., 1.);
+
+	} if (present_addr != "")  {
 		std::cout << "Presenting from addr " << present_addr << std::endl;
 		QApplication app(argc, argv);
 		gs::StreamView3d view3d(view_update_time, stream3d_dimention);
@@ -114,6 +171,7 @@ int main(int argc, char** argv) {
 
 	} else {
 
+		std::cout << "Opening GS" << std::endl;
 
 		autopilot::NormalPlainPlatform platform =
 			sim?
@@ -124,62 +182,53 @@ int main(int argc, char** argv) {
 		boost::shared_ptr<vec3_watch_stream> compass_watch(new vec3_watch_stream(platform.compass_sensor));
 		platform.compass_sensor = compass_watch;
 
-		autopilot::Cockpit cockpit(platform);
+		std::ofstream acc_file((record_dir + "/acc.stream").c_str());
+		std::ofstream compass_file((record_dir + "/compass.stream").c_str());
+		std::ofstream gyro_file((record_dir + "/gyro.stream").c_str());
+		if (record_dir != "") {
+			typedef stream::filters::RecorderPopFilter<lin_algebra::vec3f> recorder;
 
+			platform.acc_sensor.reset(new recorder(acc_file, platform.acc_sensor));
+			platform.compass_sensor.reset(new recorder(compass_file, platform.compass_sensor));
+			platform.gyro_sensor.reset(new recorder(gyro_file, platform.gyro_sensor));
+  		}
+
+		autopilot::Cockpit cockpit(platform);
 		boost::thread update_thread(update_cockpit, &cockpit);
 
-		if (present_local) {
-			std::cout << "Presenting locally" << std::endl;
+		QApplication app(argc, argv);
+		gs::StreamView3d view3d(view_update_time, stream3d_dimention);
 
-			QApplication app(argc, argv);
-			gs::StreamView3d view3d(view_update_time, stream3d_dimention);
+		// the left one
+		view3d.addAngleStream(cockpit.orientation()->get_watch_stream(), irr::core::vector3df(-20., 0., 0.));
 
-			// the left one
-			view3d.addAngleStream(cockpit.orientation()->get_watch_stream(), irr::core::vector3df(-20., 0., 0.));
+		// the mid one
+		view3d.addAngleStream(cockpit.watch_rest_orientation(), irr::core::vector3df(0., 0., 0.));
+		view3d.addVecStream(cockpit.watch_fixed_acc(), irr::core::vector3df(0., 0., 0.));
+		view3d.addVecStream(compass_watch->get_watch_stream(), irr::core::vector3df(0., 0., 0.));
 
-			// the mid one
-			view3d.addAngleStream(cockpit.watch_rest_orientation(), irr::core::vector3df(0., 0., 0.));
-			view3d.addVecStream(cockpit.watch_fixed_acc(), irr::core::vector3df(0., 0., 0.));
-			view3d.addVecStream(compass_watch->get_watch_stream(), irr::core::vector3df(0., 0., 0.));
+		// the right one
+		view3d.addAngleStream(cockpit.watch_gyro_orientation(), irr::core::vector3df(20., 0., 0.));
 
-			// the right one
-			view3d.addAngleStream(cockpit.watch_gyro_orientation(), irr::core::vector3df(20., 0., 0.));
+		// the reliable stream
+		gs::SizeStreamView view_size(cockpit.watch_rest_reliability(), view_update_time, 0., 1.);
 
-			// the reliable stream
-			gs::SizeStreamView view_size(cockpit.watch_rest_reliability(), view_update_time, 0., 1.);
-
-			// the position
-			gs::MapStreamView map_view(cockpit.position(), 1.0f, stream3d_dimention,
-							std::string("../ground_station/data/map"));
+		// the position
+		gs::MapStreamView map_view(cockpit.position(), 1.0f, stream3d_dimention,
+						std::string("../ground_station/data/map"));
 
 
-			// create the window itself
-			QHBoxLayout* layout = new QHBoxLayout();
-			layout->addWidget(&view3d);
-			layout->addWidget(&view_size);
-			layout->addWidget(&map_view);
-			QWidget* wnd = new QWidget;
-			wnd->setLayout(layout);
-			wnd->show();
-			view3d.start();
-			view_size.start();
-			app.exec();
-
-		}  else {
-			std::cout << "Exporting all data" << std::endl;
-			boost::shared_ptr<stream::TcpipClient> client = boost::make_shared<stream::TcpipClient>(present_addr, 0x6060);
-			stream::StreamConnection conn(client);
-			conn.export_pop_stream<lin_algebra::vec3f>(cockpit.watch_gyro_orientation(), "gyro_watch_orientation");
-			conn.export_pop_stream<lin_algebra::vec3f>(cockpit.watch_rest_orientation(), "watch_rest_orientation");
-			conn.export_pop_stream<lin_algebra::vec3f>(cockpit.watch_fixed_acc(), "watch_acc_sensor");
-			conn.export_pop_stream<lin_algebra::vec3f>(compass_watch->get_watch_stream(), "watch_compass_sensor");
-			conn.export_pop_stream<lin_algebra::vec3f>(cockpit.orientation(), "orientation");
-			conn.export_pop_stream<float>(cockpit.watch_rest_reliability(), "reliability");
-			conn.export_pop_stream<lin_algebra::vec2f>(cockpit.position(), "position");
-
-			// run connection and make it block
-			conn.run(false);
-		}
+		// create the window itself
+		QHBoxLayout* layout = new QHBoxLayout();
+		layout->addWidget(&view3d);
+		layout->addWidget(&view_size);
+		layout->addWidget(&map_view);
+		QWidget* wnd = new QWidget;
+		wnd->setLayout(layout);
+		wnd->show();
+		view3d.start();
+		view_size.start();
+		app.exec();
 
 	}
 	return 0;
--- projs/autopilot/src/sandbox/cockpit_test_device.cc
+++ projs/autopilot/src/sandbox/cockpit_test_device.cc
@@ -6,6 +6,7 @@
 #include <string>
 #include <iostream>
 
+typedef stream::filters::WatchFilter<lin_algebra::vec3f> vec3_watch_stream;
 
 void update_cockpit(autopilot::Cockpit* cockpit) {
 	while (true) {
@@ -21,6 +22,13 @@ int main(int argc, char** argv) {
 
 	autopilot::NormalPlainPlatform platform = autopilot::create_dgx1_platform();
 
+	// add a watch stream on the compass and acc stream
+	boost::shared_ptr<vec3_watch_stream> compass_watch(new vec3_watch_stream(platform.compass_sensor));
+	platform.compass_sensor = compass_watch;
+	boost::shared_ptr<vec3_watch_stream> acc_watch(new vec3_watch_stream(platform.acc_sensor));
+	platform.acc_sensor = acc_watch;
+
+
 	autopilot::Cockpit cockpit(platform);
 
 	boost::thread update_thread(update_cockpit, &cockpit);
@@ -31,8 +39,8 @@ int main(int argc, char** argv) {
 	stream::StreamConnection conn(client);
 	conn.export_pop_stream<lin_algebra::vec3f>(cockpit.watch_gyro_orientation(), "gyro_watch_orientation");
 	conn.export_pop_stream<lin_algebra::vec3f>(cockpit.watch_rest_orientation(), "watch_rest_orientation");
-	conn.export_pop_stream<lin_algebra::vec3f>(platform.acc_sensor->get_watch_stream(), "watch_acc_sensor");
-	conn.export_pop_stream<lin_algebra::vec3f>(platform.compass_sensor->get_watch_stream(), "watch_compass_sensor");
+	conn.export_pop_stream<lin_algebra::vec3f>(acc_watch->get_watch_stream(), "watch_acc_sensor");
+	conn.export_pop_stream<lin_algebra::vec3f>(compass_watch->get_watch_stream(), "watch_compass_sensor");
 	conn.export_pop_stream<lin_algebra::vec3f>(cockpit.orientation(), "orientation");
 	conn.export_pop_stream<float>(cockpit.watch_rest_reliability(), "reliability");
 	conn.export_pop_stream<lin_algebra::vec2f>(cockpit.position(), "position");
