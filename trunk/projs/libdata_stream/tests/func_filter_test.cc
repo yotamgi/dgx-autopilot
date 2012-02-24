590d174428366826701ef476dd8ad80ba9712569
yotam.gi <yotam.gi@2154cc09-3a30-70b6-dd6e-86fc0e90be83>
2012-02-24 07:39:32 +0000

    Added the func_push_filter class.

--- trunk/projs/libdata_stream/tests/func_filter_test.cc
+++ trunk/projs/libdata_stream/tests/func_filter_test.cc
@@ -7,7 +7,12 @@
 int basic_filt(int in) {
 	return in * 2;
 }
-TEST(GenerateTest, basic) {
+
+///////////////////////////////////////////////////////////////////////////////////////////////
+// Func Pop Stream Tests
+///////////////////////////////////////////////////////////////////////////////////////////////
+
+TEST(FuncFilter, pop_basic) {
 	boost::shared_ptr<stream::PushToPopConv<int> > stream(new stream::PushToPopConv<int>(0));
 
 	stream::FuncPopFilter<int> filtered_stream(stream, basic_filt);
@@ -20,7 +25,7 @@ TEST(GenerateTest, basic) {
 }
 
 
-TEST(GenerateTest, multitype) {
+TEST(FuncFilter, pop_multitype) {
 	boost::shared_ptr<stream::PushToPopConv<lin_algebra::vec3f> > stream(
 			new stream::PushToPopConv<lin_algebra::vec3f>(lin_algebra::vec3f())
 	);
@@ -32,3 +37,34 @@ TEST(GenerateTest, multitype) {
 	stream->set_data(v);
 	ASSERT_NEAR(filtered_stream.get_data(), 100., 0.01);
 }
+
+///////////////////////////////////////////////////////////////////////////////////////////////
+// Func Push Stream Tests
+///////////////////////////////////////////////////////////////////////////////////////////////
+
+TEST(FuncFilter, push_basic) {
+	boost::shared_ptr<stream::PushToPopConv<int> > stream(new stream::PushToPopConv<int>(0));
+
+	stream::FuncPushFilter<int> filtered_stream(stream, basic_filt);
+
+	filtered_stream.set_data(0);
+	ASSERT_EQ(stream->get_data(), 0);
+
+	filtered_stream.set_data(1);
+	ASSERT_EQ(stream->get_data(), 2);
+}
+
+
+TEST(FuncFilter, push_multitype) {
+	boost::shared_ptr<stream::PushToPopConv<float> > stream(
+			new stream::PushToPopConv<float>(0.)
+	);
+
+	stream::FuncPushFilter<lin_algebra::vec3f, float> filtered_stream(stream, lin_algebra::vec_len<lin_algebra::vec3f>);
+
+	lin_algebra::vec3f v;
+	v[0] = 100.; v[1] = 0.; v[2] = 0.;
+	filtered_stream.set_data(v);
+	ASSERT_NEAR(stream->get_data(), 100., 0.01);
+}
+
