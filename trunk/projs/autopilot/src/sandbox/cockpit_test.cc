2f94696a0e64c165eb6aa1cb4e905a894dbb2df5
yotam.gi <yotam.gi@2154cc09-3a30-70b6-dd6e-86fc0e90be83>
2012-02-25 10:30:14 +0000

    A little refactor in the cockpit test. now it looks much better, though steel ugly.

--- trunk/projs/autopilot/src/sandbox/cockpit_test.cc
+++ trunk/projs/autopilot/src/sandbox/cockpit_test.cc
@@ -48,11 +48,125 @@ boost::shared_ptr<stream::PushGenerator<T> > add_push_recorder(
 	return forwarder;
 }
 
-int main(int argc, char** argv) {
+typedef boost::shared_ptr<stream::DataPopStream<lin_algebra::vec3f> > vec3_pop_stream_ptr;
+typedef boost::shared_ptr<stream::DataPopStream<lin_algebra::vec2f> > vec2_pop_stream_ptr;
+typedef boost::shared_ptr<stream::DataPopStream<float> > 			  float_pop_stream_ptr;
+
+void open_gs(
+		vec3_pop_stream_ptr compass_sensor,
+		vec3_pop_stream_ptr acc_sensor,
+		vec3_pop_stream_ptr gyro_orientation,
+		vec3_pop_stream_ptr rest_orientation,
+		vec3_pop_stream_ptr orientation,
+		float_pop_stream_ptr reliability,
+		float_pop_stream_ptr fps,
+		vec2_pop_stream_ptr position
+	)
+{
+
 	// global parameters
 	const QSize stream3d_dimention(640, 480);
 	const float view_update_time = 0.01;
 
+	int a=0;
+	QApplication app(a, (char**)0);
+	gs::StreamView3d view3d(view_update_time, stream3d_dimention);
+
+	// the right one
+	view3d.addAngleStream(orientation, irr::core::vector3df(-20., 0., 0.));
+
+	// the mid one
+	view3d.addAngleStream(rest_orientation, irr::core::vector3df(0., 0., 0.));
+	view3d.addVecStream(acc_sensor, irr::core::vector3df(0., 0., 0.));
+	view3d.addVecStream(compass_sensor, irr::core::vector3df(0., 0., 0.));
+
+	// the right one
+	view3d.addAngleStream(gyro_orientation, irr::core::vector3df(20., 0., 0.));
+
+	// the reliable stream
+	gs::SizeStreamView view_size(reliability, view_update_time, 0., 1.);
+
+	// the gyro_fps stream
+	gs::SizeStreamView view_fps(fps, 1., 0., 4000.);
+
+	// the position
+	gs::MapStreamView map_view(position, 1.0f, stream3d_dimention,
+					std::string("../ground_station/data/map"));
+
+
+	// create the window itself
+	QHBoxLayout* layout = new QHBoxLayout();
+	layout->addWidget(&view3d);
+	layout->addWidget(&view_size);
+	if (fps) layout->addWidget(&view_fps);
+	layout->addWidget(&map_view);
+	QWidget* wnd = new QWidget;
+	wnd->setLayout(layout);
+	wnd->show();
+	view3d.start();
+	view_size.start();
+	if (fps) view_fps.start();
+	app.exec();
+}
+
+autopilot::NormalPlainPlatform record_platform(autopilot::NormalPlainPlatform platform, std::string record_dir) {
+	if (boost::filesystem::exists(record_dir) && !boost::filesystem::is_directory(record_dir)) {
+		throw std::runtime_error("The requested record dir exists and is not a directory");
+	}
+	if (!boost::filesystem::exists(record_dir)) {
+		boost::filesystem::create_directory(record_dir);
+	}
+
+	typedef boost::shared_ptr<std::ofstream> file_ptr;
+	file_ptr acc_file 			= boost::make_shared<std::ofstream>((record_dir + "/acc.stream").c_str());
+	file_ptr compass_file 		= boost::make_shared<std::ofstream>((record_dir + "/compass.stream").c_str());
+	file_ptr gyro_file 			= boost::make_shared<std::ofstream>((record_dir + "/gyro.stream").c_str());
+	file_ptr gps_pos_file 		= boost::make_shared<std::ofstream>((record_dir + "/gps_pos.stream").c_str());
+	file_ptr gps_speed_mag_file = boost::make_shared<std::ofstream>((record_dir + "/gps_speed_mag.stream").c_str());
+	file_ptr gps_speed_dir_file = boost::make_shared<std::ofstream>((record_dir + "/gps_speed_dir.stream").c_str());
+
+	typedef stream::filters::RecorderPopFilter<lin_algebra::vec3f> pop_recorder;
+
+	platform.acc_sensor.reset(new pop_recorder(acc_file, platform.acc_sensor));
+	platform.compass_sensor.reset(new pop_recorder(compass_file, platform.compass_sensor));
+	platform.gyro_sensor.reset(new pop_recorder(gyro_file, platform.gyro_sensor));
+
+	platform.gps_pos_generator = add_push_recorder<lin_algebra::vec3f>(platform.gps_pos_generator, gps_pos_file);
+	platform.gps_speed_mag_generator = add_push_recorder<float>(platform.gps_speed_mag_generator, gps_speed_mag_file);
+	platform.gps_speed_dir_generator = add_push_recorder<float>(platform.gps_speed_dir_generator, gps_speed_dir_file);
+
+	return platform;
+}
+
+autopilot::NormalPlainPlatform platform_player(std::string play_dir) {
+	if (!boost::filesystem::exists(play_dir)) {
+		throw std::runtime_error("The requested play dir does not exist");
+	}
+	typedef stream::PopStreamPlayer<lin_algebra::vec3f> vec3_pop_player;
+	typedef stream::PushStreamPlayer<lin_algebra::vec3f> vec3_push_player;
+	typedef stream::PushStreamPlayer<float> float_push_player;
+
+	typedef boost::shared_ptr<std::ifstream> ifile_ptr;
+	ifile_ptr acc_file				= boost::make_shared<std::ifstream>((play_dir + "/acc.stream").c_str());
+	ifile_ptr compass_file			= boost::make_shared<std::ifstream>((play_dir + "/compass.stream").c_str());
+	ifile_ptr gyro_file				= boost::make_shared<std::ifstream>((play_dir + "/gyro.stream").c_str());
+	ifile_ptr gps_pos_file			= boost::make_shared<std::ifstream>((play_dir + "/gps_pos.stream").c_str());
+	ifile_ptr gps_speed_mag_file	= boost::make_shared<std::ifstream>((play_dir + "/gps_speed_mag.stream").c_str());
+	ifile_ptr gps_speed_dir_file	= boost::make_shared<std::ifstream>((play_dir + "/gps_speed_dir.stream").c_str());
+
+	// create the player platform
+	autopilot::NormalPlainPlatform platform;
+	platform.acc_sensor = boost::make_shared<vec3_pop_player>(acc_file);
+	platform.gyro_sensor = boost::make_shared<vec3_pop_player>(gyro_file);
+	platform.compass_sensor = boost::make_shared<vec3_pop_player>(compass_file);
+	platform.gps_pos_generator = boost::make_shared<vec3_push_player>(gps_pos_file);
+	platform.gps_speed_dir_generator = boost::make_shared<float_push_player>(gps_speed_dir_file);
+	platform.gps_speed_mag_generator = boost::make_shared<float_push_player>(gps_speed_mag_file);
+
+	return platform;
+}
+
+int main(int argc, char** argv) {
 	// commandline parsing
 	bool sim = false;
 	std::string record_dir;
@@ -115,121 +229,9 @@ int main(int argc, char** argv) {
 	}
 
 	if (play_dir != "") {
-		if (!boost::filesystem::exists(play_dir)) {
-			throw std::runtime_error("The requested play dir does not exist");
-		}
-		typedef stream::PopStreamPlayer<lin_algebra::vec3f> vec3_pop_player;
-		typedef stream::PushStreamPlayer<lin_algebra::vec3f> vec3_push_player;
-		typedef stream::PushStreamPlayer<float> float_push_player;
 
-		typedef boost::shared_ptr<std::ifstream> ifile_ptr;
-		ifile_ptr acc_file				= boost::make_shared<std::ifstream>((play_dir + "/acc.stream").c_str());
-		ifile_ptr compass_file			= boost::make_shared<std::ifstream>((play_dir + "/compass.stream").c_str());
-		ifile_ptr gyro_file				= boost::make_shared<std::ifstream>((play_dir + "/gyro.stream").c_str());
-		ifile_ptr gps_pos_file			= boost::make_shared<std::ifstream>((play_dir + "/gps_pos.stream").c_str());
-		ifile_ptr gps_speed_mag_file	= boost::make_shared<std::ifstream>((play_dir + "/gps_speed_mag.stream").c_str());
-		ifile_ptr gps_speed_dir_file	= boost::make_shared<std::ifstream>((play_dir + "/gps_speed_dir.stream").c_str());
-
-		// create the player platform
-		autopilot::NormalPlainPlatform platform;
-		platform.acc_sensor = boost::make_shared<vec3_pop_player>(acc_file);
-		platform.gyro_sensor = boost::make_shared<vec3_pop_player>(gyro_file);
-		platform.compass_sensor = boost::make_shared<vec3_pop_player>(compass_file);
-		platform.gps_pos_generator = boost::make_shared<vec3_push_player>(gps_pos_file);
-		platform.gps_speed_dir_generator = boost::make_shared<float_push_player>(gps_speed_dir_file);
-		platform.gps_speed_mag_generator = boost::make_shared<float_push_player>(gps_speed_mag_file);
-
+		autopilot::NormalPlainPlatform platform = platform_player(play_dir);
 		std::cout << "Playing data from directory: " << play_dir << std::endl;
-
-		// add a watch stream on the compass stream
-		boost::shared_ptr<vec3_watch_stream> compass_watch(new vec3_watch_stream(platform.compass_sensor));
-		platform.compass_sensor = compass_watch;
-
-		autopilot::Cockpit cockpit(platform);
-		boost::thread update_thread(update_cockpit, &cockpit);
-		QApplication app(argc, argv);
-		gs::StreamView3d view3d(view_update_time, stream3d_dimention);
-
-		// the left one
-		view3d.addAngleStream(cockpit.orientation()->get_watch_stream(), irr::core::vector3df(-20., 0., 0.));
-
-		// the mid one
-		view3d.addAngleStream(cockpit.watch_rest_orientation(), irr::core::vector3df(0., 0., 0.));
-		view3d.addVecStream(cockpit.watch_fixed_acc(), irr::core::vector3df(0., 0., 0.));
-		view3d.addVecStream(compass_watch->get_watch_stream(), irr::core::vector3df(0., 0., 0.));
-
-		// the right one
-		view3d.addAngleStream(cockpit.watch_gyro_orientation(), irr::core::vector3df(20., 0., 0.));
-
-		// the reliable stream
-		gs::SizeStreamView view_size(cockpit.watch_rest_reliability(), view_update_time, 0., 1.);
-
-		// the position
-		gs::MapStreamView map_view(cockpit.position(), 1.0f, stream3d_dimention,
-						std::string("../ground_station/data/map"));
-
-		// create the window itself
-		QHBoxLayout* layout = new QHBoxLayout();
-		layout->addWidget(&view3d);
-		layout->addWidget(&view_size);
-		layout->addWidget(&map_view);
-		QWidget* wnd = new QWidget;
-		wnd->setLayout(layout);
-		wnd->show();
-		view3d.start();
-		view_size.start();
-		app.exec();
-
-	} if (present_addr != "")  {
-		std::cout << "Presenting from addr " << present_addr << std::endl;
-		QApplication app(argc, argv);
-		gs::StreamView3d view3d(view_update_time, stream3d_dimention);
-
-		boost::shared_ptr<stream::TcpipServer> server = boost::make_shared<stream::TcpipServer>(present_addr, 0x6060);
-		stream::StreamConnection conn(server);
-
-		typedef stream::DataPopStream<lin_algebra::vec3f> vs;
-		typedef stream::DataPopStream<lin_algebra::vec2f> v2s;
-		typedef stream::DataPopStream<float> fs;
-
-		// the left one
-		view3d.addAngleStream(conn.import_pop_stream<vs::type>("orientation"), irr::core::vector3df(-20., 0., 0.));
-
-		// the mid one
-		view3d.addAngleStream(conn.import_pop_stream<vs::type>("watch_rest_orientation"), irr::core::vector3df(0., 0., 0.));
-		view3d.addVecStream(conn.import_pop_stream<vs::type>("watch_acc_sensor"), irr::core::vector3df(0., 0., 0.));
-		view3d.addVecStream(conn.import_pop_stream<vs::type>("watch_compass_sensor"), irr::core::vector3df(0., 0., 0.));
-
-		// the right one
-		view3d.addAngleStream(conn.import_pop_stream<vs::type>("gyro_watch_orientation"), irr::core::vector3df(20., 0., 0.));
-
-		// the reliable stream
-		gs::SizeStreamView view_size(conn.import_pop_stream<fs::type>("reliability"), view_update_time, 0., 1.);
-
-		// the position
-		gs::MapStreamView map_view(conn.import_pop_stream<v2s::type>("position"), 1.0f, stream3d_dimention,
-				std::string("../ground_station/data/map"));
-
-		// create the window itself
-		QHBoxLayout* layout = new QHBoxLayout();
-		layout->addWidget(&view3d);
-		layout->addWidget(&view_size);
-		layout->addWidget(&map_view);
-		QWidget* wnd = new QWidget;
-		wnd->setLayout(layout);
-		wnd->show();
-		view3d.start();
-		view_size.start();
-		app.exec();
-
-	} else {
-
-		std::cout << "Opening GS" << std::endl;
-
-		autopilot::NormalPlainPlatform platform =
-			sim?
-				autopilot::create_dgx1_simulator_platform(boost::make_shared<stream::TcpipServer>(sim_addr, 0x6060)):
-				autopilot::create_dgx1_platform();
 
 		// add a watch stream on the compass stream
 		boost::shared_ptr<vec3_watch_stream> compass_watch(new vec3_watch_stream(platform.compass_sensor));
@@ -241,76 +243,71 @@ int main(int argc, char** argv) {
 		);
 		platform.gyro_sensor = fpsed_gyro;
 
+		autopilot::Cockpit cockpit(platform);
+		boost::thread update_thread(update_cockpit, &cockpit);
+
+		open_gs(compass_watch->get_watch_stream(),
+				cockpit.watch_fixed_acc(),
+				cockpit.watch_gyro_orientation(),
+				cockpit.watch_rest_orientation(),
+				cockpit.orientation()->get_watch_stream(),
+				cockpit.watch_rest_reliability(),
+				fpsed_gyro->get_fps_stream(),
+				cockpit.position()
+		);
+
+	} if (present_addr != "")  {
+		std::cout << "Presenting from addr " << present_addr << std::endl;
+
+		boost::shared_ptr<stream::TcpipServer> server = boost::make_shared<stream::TcpipServer>(present_addr, 0x6060);
+		stream::StreamConnection conn(server);
+
+		open_gs(
+				conn.import_pop_stream<lin_algebra::vec3f>("watch_compass_sensor"),
+				conn.import_pop_stream<lin_algebra::vec3f>("watch_acc_sensor"),
+				conn.import_pop_stream<lin_algebra::vec3f>("gyro_watch_orientation"),
+				conn.import_pop_stream<lin_algebra::vec3f>("watch_rest_orientation"),
+				conn.import_pop_stream<lin_algebra::vec3f>("orientation"),
+				conn.import_pop_stream<float>("reliability"),
+				float_pop_stream_ptr(),
+				conn.import_pop_stream<lin_algebra::vec2f>("position")
+		);
+
+	} else {
+
+		std::cout << "Opening GS" << std::endl;
+
+		autopilot::NormalPlainPlatform platform =
+			sim?
+				autopilot::create_dgx1_simulator_platform(boost::make_shared<stream::TcpipServer>(sim_addr, 0x6060)):
+				autopilot::create_dgx1_platform();
+
 		if (record_dir != "") {
-
-			if (boost::filesystem::exists(record_dir) && !boost::filesystem::is_directory(record_dir)) {
-				throw std::runtime_error("The requested record dir exists and is not a directory");
-			}
-			if (!boost::filesystem::exists(record_dir)) {
-				boost::filesystem::create_directory(record_dir);
-			}
-
-			typedef boost::shared_ptr<std::ofstream> file_ptr;
-			file_ptr acc_file 			= boost::make_shared<std::ofstream>((record_dir + "/acc.stream").c_str());
-			file_ptr compass_file 		= boost::make_shared<std::ofstream>((record_dir + "/compass.stream").c_str());
-			file_ptr gyro_file 			= boost::make_shared<std::ofstream>((record_dir + "/gyro.stream").c_str());
-			file_ptr gps_pos_file 		= boost::make_shared<std::ofstream>((record_dir + "/gps_pos.stream").c_str());
-			file_ptr gps_speed_mag_file = boost::make_shared<std::ofstream>((record_dir + "/gps_speed_mag.stream").c_str());
-			file_ptr gps_speed_dir_file = boost::make_shared<std::ofstream>((record_dir + "/gps_speed_dir.stream").c_str());
-
-			typedef stream::filters::RecorderPopFilter<lin_algebra::vec3f> pop_recorder;
-
-			platform.acc_sensor.reset(new pop_recorder(acc_file, platform.acc_sensor));
-			platform.compass_sensor.reset(new pop_recorder(compass_file, platform.compass_sensor));
-			platform.gyro_sensor.reset(new pop_recorder(gyro_file, platform.gyro_sensor));
-
-			platform.gps_pos_generator = add_push_recorder<lin_algebra::vec3f>(platform.gps_pos_generator, gps_pos_file);
-			platform.gps_speed_mag_generator = add_push_recorder<float>(platform.gps_speed_mag_generator, gps_speed_mag_file);
-			platform.gps_speed_dir_generator = add_push_recorder<float>(platform.gps_speed_dir_generator, gps_speed_dir_file);
+			platform = record_platform(platform, record_dir);
   		}
+
+		// add a watch stream on the compass stream
+		boost::shared_ptr<vec3_watch_stream> compass_watch(new vec3_watch_stream(platform.compass_sensor));
+		platform.compass_sensor = compass_watch;
+
+		// add fps stream to the gyro stream
+		boost::shared_ptr<stream::filters::FpsFilter<lin_algebra::vec3f> > fpsed_gyro(
+				new stream::filters::FpsFilter<lin_algebra::vec3f>(platform.gyro_sensor)
+		);
+		platform.gyro_sensor = fpsed_gyro;
 
 		autopilot::Cockpit cockpit(platform);
 		boost::thread update_thread(update_cockpit, &cockpit);
 
-		QApplication app(argc, argv);
-		gs::StreamView3d view3d(view_update_time, stream3d_dimention);
-
-		// the left one
-		view3d.addAngleStream(cockpit.orientation()->get_watch_stream(), irr::core::vector3df(-20., 0., 0.));
-
-		// the mid one
-		view3d.addAngleStream(cockpit.watch_rest_orientation(), irr::core::vector3df(0., 0., 0.));
-		view3d.addVecStream(cockpit.watch_fixed_acc(), irr::core::vector3df(0., 0., 0.));
-		view3d.addVecStream(compass_watch->get_watch_stream(), irr::core::vector3df(0., 0., 0.));
-
-		// the right one
-		view3d.addAngleStream(cockpit.watch_gyro_orientation(), irr::core::vector3df(20., 0., 0.));
-
-		// the reliable stream
-		gs::SizeStreamView view_size(cockpit.watch_rest_reliability(), view_update_time, 0., 1.);
-
-		// the gyro_fps stream
-		gs::SizeStreamView view_fps(fpsed_gyro->get_fps_stream(), 1., 0., 4000.);
-
-		// the position
-		gs::MapStreamView map_view(cockpit.position(), 1.0f, stream3d_dimention,
-						std::string("../ground_station/data/map"));
-
-
-		// create the window itself
-		QHBoxLayout* layout = new QHBoxLayout();
-		layout->addWidget(&view3d);
-		layout->addWidget(&view_size);
-		layout->addWidget(&view_fps);
-		layout->addWidget(&map_view);
-		QWidget* wnd = new QWidget;
-		wnd->setLayout(layout);
-		wnd->show();
-		view3d.start();
-		view_size.start();
-		view_fps.start();
-		app.exec();
-
+		open_gs(compass_watch->get_watch_stream(),
+				cockpit.watch_fixed_acc(),
+				cockpit.watch_gyro_orientation(),
+				cockpit.watch_rest_orientation(),
+				cockpit.orientation()->get_watch_stream(),
+				cockpit.watch_rest_reliability(),
+				fpsed_gyro->get_fps_stream(),
+				cockpit.position()->get_watch_stream()
+		);
 	}
 	return 0;
 
