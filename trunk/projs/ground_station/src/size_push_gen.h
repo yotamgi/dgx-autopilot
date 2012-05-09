bb303603fe95b1e91bd83ba9aef2bebe81560c0c
yotam.gi <yotam.gi@2154cc09-3a30-70b6-dd6e-86fc0e90be83>
2012-05-09 21:24:45 +0000

    Added the sizePushGen class.

---
+++ trunk/projs/ground_station/src/size_push_gen.h
@@ -0,0 +1,44 @@
+#ifndef SIZE_PUSH_GEN_H_
+#define SIZE_PUSH_GEN_H_
+
+#include <stream/data_push_stream.h>
+#include <boost/shared_ptr.hpp>
+#include <QtGui/QtGui>
+
+namespace gs {
+
+
+class SizePushGen : public QWidget {
+	Q_OBJECT
+public:
+	typedef stream::DataPushStream<float> size_stream;
+
+	enum Style {
+		VERTICAL_DIAGRAM,
+		HORIZONTAL_DIAGRAM,
+	};
+
+	SizePushGen(boost::shared_ptr<size_stream> size_stream,
+				   std::string stream_name,
+	   	   	   	   float max_size,
+	   	   	   	   float min_size,
+	   	   	   	   float start_from,
+	   	   	   	   Style style = VERTICAL_DIAGRAM);
+
+public slots:
+
+	void value_changed(int value);
+
+private:
+
+	boost::shared_ptr<size_stream> m_size_stream;
+
+	float m_coeffecient;
+	float m_min;
+	QSlider* m_slider;
+};
+
+} // namespace gs
+
+
+#endif /* SIZE_PUSH_GEN_H_ */
