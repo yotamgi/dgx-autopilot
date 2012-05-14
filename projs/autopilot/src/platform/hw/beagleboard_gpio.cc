fb5e762e2e39f11392560be3090fa334c7da7b4b
yotam.gi <yotam.gi@2154cc09-3a30-70b6-dd6e-86fc0e90be83>
2012-05-14 17:32:21 +0000

    Added the bb gpio class.

---
+++ projs/autopilot/src/platform/hw/beagleboard_gpio.cc
@@ -0,0 +1,61 @@
+#include "beagleboard_gpio.h"
+#include <sstream>
+#include <fcntl.h>
+#include <stdio.h>
+
+namespace autopilot {
+
+const std::string GPIO_PATH = "/sys/class/gpio/";
+
+GpioPin::GpioPin(unsigned int pin_num):
+		m_curr_state(false)
+{
+	std::stringstream pin_num_ss;
+	pin_num_ss << pin_num;
+
+	int master_fd = open((GPIO_PATH + "export").c_str(), O_WRONLY);
+
+	if (master_fd < 0) {
+		perror("");
+		throw SensorException("Could not open master gpio file");
+	}
+
+	int ans = write(master_fd, pin_num_ss.str().c_str(), pin_num_ss.str().size());
+	if (ans < 0) {
+		perror("");
+		throw SensorException("Could not write to master gpio file");
+	}
+	close(master_fd);
+
+	m_gpio_fd = open((GPIO_PATH + "gpio" + pin_num_ss.str()).c_str(), O_WRONLY);
+
+	if (m_gpio_fd < 0) {
+		perror("");
+		throw SensorException("Could not open specific gpio file");
+	}
+
+
+	*this = m_curr_state;
+}
+
+GpioPin::~GpioPin() {
+	close(m_gpio_fd);
+}
+
+void GpioPin::operator = (bool state) {
+	m_curr_state = state;
+	int ans = write(m_gpio_fd, m_curr_state? "high":"low", m_curr_state? 4:3);
+	if (ans < 0) {
+		perror("");
+		throw SensorException("Could not write to master gpio file");
+	}
+
+}
+
+GpioPin::operator bool () {
+	return m_curr_state;
+}
+
+
+}  // namespace autopilot
+
