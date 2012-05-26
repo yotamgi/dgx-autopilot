6b9961152aaf26cfb7df79fa094b1ee7fcbdc2ce
yotam.gi <yotam.gi@2154cc09-3a30-70b6-dd6e-86fc0e90be83>
2012-05-26 13:00:27 +0000

    Added one more feature to the adc platform

--- projs/autopilot/src/platform/hw/ads1115_adc.cc
+++ projs/autopilot/src/platform/hw/ads1115_adc.cc
@@ -1,28 +1,40 @@
 #include "ads1115_adc.h"
 #include <stdint.h>
 #include <sstream>
+#include <iostream>
 
 namespace autopilot {
 
-Ads1115_ADC::Ads1115_ADC(Ads1115_ADC::VoltageRange v_range, size_t device_i2c_num):
+Ads1115_ADC::Ads1115_ADC(size_t device_i2c_num):
 			I2C_ADDRESS(0x48),
 	        READ_DATA_REG(0x00),
 	        CONFIG_REG(0x01),
 			m_i2c(device_i2c_num, I2C_ADDRESS)
 {
-	// configuration
-	m_config_word = 0x4083 + (v_range << 9);
-	m_curr_mux = 0;
-	m_i2c.write_num<uint16_t>(CONFIG_REG, m_config_word | (m_curr_mux << 12)); 
+
+	for (size_t i=0; i<4; i++) {
+		m_channel_ranges[i] = V_6_144;
+	}
+
+	// configuration - set the default channel to 0
+	m_config_word = 0x4083;
+	m_curr_mux = 0xff;
+}
+
+void Ads1115_ADC::set_channel_range(uint8_t channel, Ads1115_ADC::VoltageRange v_range) {
+	m_channel_ranges[channel] = v_range;	
 }
 
 int16_t Ads1115_ADC::read_channel(uint8_t channel) {
 
 	// pick the channel if needed
 	if (m_curr_mux != channel) {
-		m_i2c.write_num<uint16_t>(CONFIG_REG, m_config_word | (channel << 12)); 
+		std::cout << "SETING TO CHANNEL " << channel << " WITH RANGE " << m_channel_ranges[channel];
+		m_i2c.write_num<uint16_t>(CONFIG_REG, m_config_word | (channel << 12) | (m_channel_ranges[channel] << 9)); 
+		std::cout << std::hex << " CONF " << (m_config_word | (channel << 12) | (m_channel_ranges[channel] << 9)) << std::endl;
 		m_curr_mux = channel;
 	}
+
 
 	// read the data
 	return m_i2c.read_num<int16_t>(READ_DATA_REG);
--- projs/autopilot/src/platform/hw/ads1115_adc.h
+++ projs/autopilot/src/platform/hw/ads1115_adc.h
@@ -13,7 +13,7 @@ public:
 		V_6_144, V_4_096, V_2_048, V_1_024, V_0_512, V_0_256
 	};
 
-	Ads1115_ADC(VoltageRange v_range, size_t device_i2c_num);
+	Ads1115_ADC(size_t device_i2c_num);
 
 	virtual ~Ads1115_ADC() {}
 
@@ -22,6 +22,8 @@ public:
 	 * @param channel runs from 0 to 3
 	 */
 	int16_t read_channel(uint8_t channel);
+
+	void set_channel_range(uint8_t channel, VoltageRange v_range);
 
 private:
 
@@ -38,6 +40,8 @@ private:
 
 	uint8_t m_curr_mux;
 	uint16_t m_config_word;
+
+	VoltageRange m_channel_ranges[4];
 
 	/**
 	 * the i2c device communication object.
