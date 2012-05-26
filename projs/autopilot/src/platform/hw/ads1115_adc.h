1c2a55743ad869ec106e62717385531c58933038
yotam.gi <yotam.gi@2154cc09-3a30-70b6-dd6e-86fc0e90be83>
2012-05-26 12:22:05 +0000

    added the adc and its test.

---
+++ projs/autopilot/src/platform/hw/ads1115_adc.h
@@ -0,0 +1,50 @@
+
+#ifndef ADS1115_ADC_H_
+#define ADS1115_ADC_H_
+
+#include "platform/hw/i2c_interface.h"
+
+namespace autopilot {
+
+class Ads1115_ADC {
+public:
+
+	enum VoltageRange {
+		V_6_144, V_4_096, V_2_048, V_1_024, V_0_512, V_0_256
+	};
+
+	Ads1115_ADC(VoltageRange v_range, size_t device_i2c_num);
+
+	virtual ~Ads1115_ADC() {}
+
+	/**
+	 * Reads the data from the analog channel.
+	 * @param channel runs from 0 to 3
+	 */
+	int16_t read_channel(uint8_t channel);
+
+private:
+
+	/**
+	 * fixed values.
+	 */
+	const uint8_t I2C_ADDRESS;
+
+	/**
+	 * Internal registers
+	 */
+	const uint8_t READ_DATA_REG;
+	const uint8_t CONFIG_REG;
+
+	uint8_t m_curr_mux;
+	uint16_t m_config_word;
+
+	/**
+	 * the i2c device communication object.
+	 */
+	I2C_Interface m_i2c;
+};
+
+}  // namespace autopilot
+
+#endif /* ADS1115_ADC_H_ */
