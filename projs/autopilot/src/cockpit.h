bcc3bd334bfe8a5b7758b244d6751065e3b583d0
yotam.gi <yotam.gi@2154cc09-3a30-70b6-dd6e-86fc0e90be83>
2012-06-06 19:52:02 +0000

    Added the calibration options. STAGE 1

--- projs/autopilot/src/cockpit.h
+++ projs/autopilot/src/cockpit.h
@@ -13,15 +13,35 @@ namespace autopilot {
 
 typedef stream::filters::WatchFilter<lin_algebra::vec3f> vec3_watch_stream;
 
+
 /**
  * Specific cockpit for normal plains.
  */
 class Cockpit : public NormalPlainCockpit {
 public:
 
+	struct calibration_data {
+		lin_algebra::vec3f orientation;
+		float alt;
+	};
+
 	Cockpit(NormalPlainPlatform platform);
 	virtual ~Cockpit();
 
+	/**
+	 * Calibrate function.
+	 * This one reads the data from the sensors, understand the calibration
+	 * data and calibrate the cocktpit.
+	 * @return the calibration data that the plane was calibrated by.
+	 */
+	const calibration_data& calibrate();
+
+	/**
+	 * Calibrate function
+	 * This function gets calibration data and by that calibrated the plane.
+	 * @param calibration - the calibration data to calibrate the plane by.
+	 */
+	void calibrate(const calibration_data& calibration);
 
 	/* NormalPlainCockpit Implementation */
 
@@ -55,13 +75,34 @@ public:
 
 
 private:
+
+	template <typename T>
+	class CalibrationFilter : public stream::StreamPopFilter<T> {
+	public:
+		CalibrationFilter(boost::shared_ptr<stream::DataPopStream<T> > filtered_stream):
+			stream::StreamPopFilter<T>(filtered_stream) {}
+
+		T get_data()
+		{ return stream::StreamPopFilter<T>::m_generator->get_data() + m_calibration_constant; }
+
+		void calibrate(T calibration_constatnt)
+		{ m_calibration_constant = calibration_constatnt; }
+
+	private:
+		T m_calibration_constant;
+	};
+
 	NormalPlainPlatform m_platform;
 
-	boost::shared_ptr<vec3_watch_stream> 	m_orientation;
+	boost::shared_ptr<vec3_watch_stream>	m_orientation;
 	boost::shared_ptr<vec3_stream> 			m_gyro_orientation;
 	boost::shared_ptr<vec3_stream> 			m_rest_orientation;
 	boost::shared_ptr<float_stream> 		m_rest_reliability;
 	boost::shared_ptr<vec3_stream> 			m_fixed_acc;
+	boost::shared_ptr<float_watch_stream> 	m_alt_stream;
+
+	boost::shared_ptr<CalibrationFilter<float> > 				m_alt_calibration;
+	boost::shared_ptr<CalibrationFilter<lin_algebra::vec3f> > 	m_orientation_calibration;
 
 	boost::shared_ptr<stream::PushToPopConv<lin_algebra::vec3f> > m_gps_pos;
 	boost::shared_ptr<stream::PushToPopConv<float> > m_gps_speed_dir;
