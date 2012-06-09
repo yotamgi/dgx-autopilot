d365958e95146858d39b6695954d93ce62c5125a
yotam.gi <yotam.gi@2154cc09-3a30-70b6-dd6e-86fc0e90be83>
2012-06-09 16:33:45 +0000

    Added the play and pause buttons to the cockpit_player.

--- trunk/apps/cockpit_player/cockpit_player.h
+++ trunk/apps/cockpit_player/cockpit_player.h
@@ -24,12 +24,16 @@ signals:
 
 public slots:
 
+	void play(bool state);
+	void pause();
 
 private:
 
 	typedef stream::PopStreamPlayer<lin_algebra::vec3f> vec3_pop_player;
 	typedef stream::PushStreamPlayer<lin_algebra::vec3f> vec3_push_player;
 	typedef stream::PushStreamPlayer<float> float_push_player;
+
+	void update_cockpit();
 
 	QApplication m_app;
 
@@ -41,6 +45,10 @@ private:
     boost::shared_ptr<float_push_player> 	m_gps_speed_mag_generator_player;
 
     boost::shared_ptr<autopilot::Cockpit> m_cockpit;
+
+    boost::thread m_update_cockpit_thread;
+
+    QAction* m_play_action;
 };
 
 #endif /* GROUND_STATION_H_ */
