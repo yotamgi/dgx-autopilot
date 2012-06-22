1706f6114dd976bd67c59334f1e98f9727d76ea8
yotam.gi <yotam.gi@2154cc09-3a30-70b6-dd6e-86fc0e90be83>
2012-06-22 07:21:31 +0000

    Added the on-start calibration to the pitot sensor.

--- projs/autopilot/src/platform/hw/mpxv7002_pitot.h
+++ projs/autopilot/src/platform/hw/mpxv7002_pitot.h
@@ -13,15 +13,20 @@ namespace autopilot {
 class Mpxv7002Pitot : public stream::DataPopStream<float> {
 public:
 
-	Mpxv7002Pitot(boost::shared_ptr<Ads1115_ADC> adc, uint8_t channel):m_adc(adc), m_channel(channel) {
+	Mpxv7002Pitot(boost::shared_ptr<Ads1115_ADC> adc, uint8_t channel):
+		m_adc(adc), m_channel(channel)
+	{
 		m_adc->set_channel_range(m_channel, Ads1115_ADC::V_4_096);
+
+		// assume that we start at airspeed 0, so check the zero voltage
+		m_zero_calibration_voltage = (float(m_adc->read_channel(m_channel))/float(0x7fff))*4.096;
 	}
 
 	virtual ~Mpxv7002Pitot() {}
 
 	float get_data() { 
 		float data_volts = (float(m_adc->read_channel(m_channel))/float(0x7fff))*4.096;
-		float pressure_diff_pa = 100*(data_volts - 2.49);
+		float pressure_diff_pa = 100*(data_volts - m_zero_calibration_voltage);
 		if (pressure_diff_pa < 0.001) pressure_diff_pa = 0.001;
 		return sqrt(2*pressure_diff_pa/1.2041);
 	}
@@ -31,6 +36,7 @@ private:
 	boost::shared_ptr<Ads1115_ADC> m_adc;
 	uint8_t m_channel;
 
+	float m_zero_calibration_voltage;
 };
 
 }  // namespace autopilot
