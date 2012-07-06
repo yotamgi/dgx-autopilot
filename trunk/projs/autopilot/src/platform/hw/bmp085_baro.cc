6321b51436a41b287b957977c8011837b473c1fe
yotam.gi <yotam.gi@2154cc09-3a30-70b6-dd6e-86fc0e90be83>
2012-07-06 13:18:31 +0000

    And now the barometer actually works...

--- trunk/projs/autopilot/src/platform/hw/bmp085_baro.cc
+++ trunk/projs/autopilot/src/platform/hw/bmp085_baro.cc
@@ -4,13 +4,11 @@
 
 namespace autopilot {
 
-Bmp085baro::Bmp085baro(size_t device_i2c_num, bool continues_mode, float see_level_pressure):
+Bmp085baro::Bmp085baro(size_t device_i2c_num, bool continious_mode):
 			I2C_ADDRESS(0x77),
-			m_see_level_pressure(see_level_pressure),
-			m_continous_mode(continues_mode),
+			m_continious_mode(continious_mode),
 			m_i2c(device_i2c_num, I2C_ADDRESS)
 {
-
 	m_ac1 = m_i2c.read_num<int16_t>(0xaa);   // 408;   
 	m_ac2 = m_i2c.read_num<int16_t>(0xac);   // -72;   
 	m_ac3 = m_i2c.read_num<int16_t>(0xae);   // -14383;
@@ -21,37 +19,64 @@ Bmp085baro::Bmp085baro(size_t device_i2c_num, bool continues_mode, float see_lev
 	m_b2  = m_i2c.read_num<int16_t>(0xb8);   // 4;     
 	m_mb  = m_i2c.read_num<int16_t>(0xba);   // -32768;
 	m_mc  = m_i2c.read_num<int16_t>(0xbc);   // -8711; 
-	m_md  = m_i2c.read_num<int16_t>(0xbe);   // 2868;
+	m_md  = m_i2c.read_num<int16_t>(0xbe);   // 2868;  
+
 
 	update_temperature();
-	start_updating_pressure();
-	usleep(25500);
-	update_pressure();
 }
+
+float Bmp085baro::get_data() {
+	
+
+	if (m_continious_mode) {
+		if (m_update_temp_timer.passed() > TEMP_UPDATE_TIME) { 
+			update_temperature();
+			m_update_temp_timer.reset();
+			prepare_pressure();
+			m_update_pres_timer.reset();
+		}
+
+		if (m_update_pres_timer.passed() > PRES_UPDATE_TIME) {
+			read_pressure();
+			calc_alt();
+			prepare_pressure();
+			m_update_pres_timer.reset();
+		}
+	}
+
+	else {
+		update_temperature();
+		prepare_pressure();
+		usleep(25500);
+		read_pressure();
+		calc_alt();
+	}
+
+	return m_curr_alt;
+}
+
 
 void Bmp085baro::update_temperature() {
+	//read temperature value
+	//datasheet start reading temp 
+	m_i2c.write_num<uint8_t>(0xF4, 0x2E);
 
-	// start temperature conversion
-	m_i2c.write_num<uint8_t>(CONTROL_REGISTER, CONVERT_TEMPERATURE);
-
-	// sleep 4.5ms
-	usleep(4500);
-	m_current_ut = m_i2c.read_num<uint16_t>(DATA_READ);
+	usleep(7000);
+	m_curr_ut = m_i2c.read_num<uint16_t>(0xf6);
 }
 
-void Bmp085baro::start_updating_pressure() {
-	// start pressure conversion
-	m_i2c.write_num<uint8_t>(CONTROL_REGISTER, CONVERT_TEMPERATURE+(RESOLUTION<<6));
+void Bmp085baro::prepare_pressure() {
+	// tell the sensor to start reading pressure
+	m_i2c.write_num<uint8_t>(0xF4, 0x34+(RESOLUTION<<6));
 }
 
-void Bmp085baro::update_pressure() {
 
-	std::string raw =  m_i2c.read_buff(DATA_READ, 3);
-
-	m_current_up = (raw[2] + (raw[1] << 8) + (raw[0] << 16))>>(8-RESOLUTION);
+void Bmp085baro::read_pressure() {
+	std::string raw =  m_i2c.read_buff(0xF6, 3);
+	m_curr_up = (raw[2] + (raw[1] << 8) + (raw[0] << 16))>>(8-RESOLUTION);
 }
 
-float Bmp085baro::calculate_pressure() {
+void Bmp085baro::calc_alt() {
 	int32_t x1;
 	int32_t x2;
 	int32_t x3;
@@ -60,11 +85,13 @@ float Bmp085baro::calculate_pressure() {
 	int16_t b5;
 	int32_t b6;
 	uint32_t b7;
+//	int32_t t;
 	int32_t p;
 
-  	x1 = ((m_current_ut-m_ac6)*m_ac5)>>15;
+  	x1 = ((m_curr_ut-m_ac6)*m_ac5)>>15;
   	x2 = (m_mc<<11)/(x1+m_md);
   	b5 = x1+x2;
+//	t  = (b5+8)>>4;
 
 	// calc true pressure
 	b6 = b5 - 4000;
@@ -76,7 +103,7 @@ float Bmp085baro::calculate_pressure() {
 	x2 = (m_b1*(b6*b6>>12))>>16;
 	x3 = ((x1 +x2)+2)>>2;
 	b4 = m_ac4*(uint32_t)(x3+32768)>>15;
-	b7 = ((uint32_t)m_current_up-b3)*(50000>>RESOLUTION);
+	b7 = ((uint32_t)m_curr_up-b3)*(50000>>RESOLUTION);
 	if (b7<0x80000000) 	p  = (b7*2)/b4;
 	else				p  = (b7/b4)*2;
 	x1 = (p>>8)*(p>>8);
@@ -85,39 +112,8 @@ float Bmp085baro::calculate_pressure() {
 
 	p  = p + ((x1+x2+3791)>>4);	
 
-	return (m_see_level_pressure - p)/10.;
+	m_curr_alt = (100500. - p)/10.;
 }
 
-float Bmp085baro::get_data() {
-
-	// update the temperature if needed
-	if (m_temp_update_timer.passed() > TEMPERATURE_UPDATE_TIME) {
-		update_temperature();
-		m_temp_update_timer.reset();
-	}
-
-	// continious mode
-	if (m_continous_mode) {
-
-		if (m_pressure_update_timer.passed() > PRESSURE_UPDATE_TIME) {
-			update_pressure();
-			start_updating_pressure();
-			m_pressure_update_timer.reset();
-		}
-
-		return calculate_pressure();
-	}
-
-	// non-continious mode
-	else {
-
-		start_updating_pressure();
-		usleep(25500);
-		update_pressure();
-
-		return calculate_pressure();
-	}
-
-}
 
 }  // namespace autopilot
--- trunk/projs/autopilot/src/sandbox/baro_test.cc
+++ trunk/projs/autopilot/src/sandbox/baro_test.cc
@@ -1,10 +1,10 @@
-#include <platform/hw/bmp085_baro.h>
+#include <platform/hw/bmp085_baro.h> 
 #include <iostream>
 
 
 int main() {
 
-	autopilot::Bmp085baro b(2);
+	autopilot::Bmp085baro b(2, true);
 	while(true) {
 		std::cout << b.get_data() << std::endl;
 	}
